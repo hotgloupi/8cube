d470dd921d6f50ea8fdcd5fc22f46b50c0fe94f9
Raphaël Londeix <raphael.londeix@gmail.com>
2012-09-17 01:58:27 +0200

    enable cubeapp c++

--- setup.sh
+++ setup.sh
@@ -23,23 +23,34 @@ usage()
 }
 
 [ -z "$1" ] && usage
-[ -d "$1" ] || fatal "Error: $1 is not a directory"
+[ -d "$1" ] || fatal "Error: $1: not a directory"
 
 [ -z "$2" ] && usage
-[ -e "$2" -a ! -d "$2" ] && fatal "Error: $2 exists and is not a directory"
+[ -e "$2" -a ! -d "$2" ] && fatal "Error: $2: already exists but not a directory"
 
 BUILD_DIR="$1"
 DEST_DIR="$2"
 
 echo "-- Setup of $BUILD_DIR in $DEST_DIR"
 
-mkdir -p "$DEST_DIR"
+RELEASE_DATE_FILE="$DEST_DIR/share/8cube/.release"
+echo $RELEASE_DATE_FILE
+
+[ -d "$DEST_DIR" ] && [ ! -f "$RELEASE_DATE_FILE" ] && \
+	fatal "Error: $DEST_DIR: exists but not a release directory"
+
+rm -rf "$DEST_DIR"
+mkdir -p "`dirname $RELEASE_DATE_FILE`"
 cp -rv "$BUILD_DIR/release"/* "$DEST_DIR"
 find "$DEST_DIR" -name '*.o' -exec rm -fv {} \;
 find "$DEST_DIR" -name '*.a' -exec rm -fv {} \;
 find "$DEST_DIR" -name 'Tupfile' -exec rm -v {} \;
 find "$DEST_DIR" -name '__pycache__' -exec rm -frv {} \;
-find "$DEST_DIR" -name 'libetc.so' -exec rm -frv {} \;
-find "$DEST_DIR" -name 'libcube.so' -exec rm -frv {} \;
 find "$DEST_DIR" -executable -and -type f -exec strip -s {} \;
+date > "$RELEASE_DATE_FILE"
+echo "-- Release built at `cat $RELEASE_DATE_FILE`"
+echo
 
+
+[ -n "$(find "$DEST_DIR" \( -name 'libetc.so' -o -name 'libcube.so' -o -name 'libcubeapp.so' \))" ] && \
+	echo "WARNING: release contains core dynamic libraries"
