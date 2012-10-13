550577bbbb9de318c48379e17b2c619524659918
Raphaël Londeix <raphael.londeix@gmail.com>
2012-10-13 15:05:02 +0200

    improve: simplified project config

--- .config/project.py
+++ .config/project.py
@@ -8,98 +8,6 @@ VERSION_NAME = "prototype"
 from tupcfg import Source
 from tupcfg import Target
 from tupcfg import Command
-from tupcfg import Build
-
-class BuildObject(Command):
-    action = "Building CXX object"
-    def __init__(self, compiler, source):
-        if not isinstance(source, Source):
-            raise Exception(
-                "Cannot build object from '%s' (of type %s)" % (source, type(source))
-            )
-        self.compiler = compiler
-        super(BuildObject, self).__init__(source)
-
-    def command(self, **kw):
-        from os.path import join
-        from tupcfg.tools import cleanabspath
-        return (
-            [self.compiler.binary] +
-            self.compiler.flags +
-            list(('-I' + i) for i in self.compiler.include_directories),
-            '-I', cleanabspath(join(kw['build'].root_directory, 'src')),
-            '-fPIC',
-            '-c', self.dependencies[0],
-            '-o', kw['target'],
-        )
-
-class LinkCommand(Command):
-    class LinkFlag:
-        def __init__(self, lib, method):
-            self.lib = lib
-            self.method = method
-
-        def shell_string(self, **kw):
-            return self.method(self.lib, **kw)
-
-    def __init__(self, compiler, objects, flags=[], link_libraries=[]):
-        self.compiler = compiler
-        self.flags = flags
-        self.linker_flags = []
-        from os.path import dirname, abspath
-        closure = set()
-        def add_rpath(lib, **kw):
-            p = '-Wl,-rpath=\\$ORIGIN/' + dirname(lib.relpath(kw['target'], **kw))
-            if p in closure:
-                return ''
-            else:
-                closure.add(p)
-                return p
-
-        for lib in link_libraries:
-            if isinstance(lib, Target):
-                self.linker_flags.append(self.LinkFlag(lib, add_rpath))
-                self.linker_flags.append(lib)
-            else:
-                if lib.directories:
-                    self.linker_flags.extend('-L' + dir_ for dir_ in lib.directories)
-                self.linker_flags.extend(('-l' + name) for name in lib.names)
-        self.link_libraries = link_libraries
-        super(LinkCommand, self).__init__(objects)
-
-
-class LinkExecutable(LinkCommand):
-    action = "Linking executable"
-
-
-    def command(self, **kw):
-        return (
-            [self.compiler.binary] +
-            self.flags +
-            self.dependencies +
-            ['-o', kw['target']] +
-            self.compiler.linker_flags +
-            self.linker_flags
-        )
-
-class LinkLibrary(LinkCommand):
-    action = "Linking library"
-
-    def command(self, **kw):
-        from os.path import basename
-        def set_soname(lib, **kw):
-            return '-Wl,-soname=' + basename(lib.name)
-
-        return (
-            [self.compiler.binary] +
-            self.compiler.linker_flags +
-            self.flags +
-            self.dependencies +
-            ['-shared', '-o', kw['target']] +
-            [self.LinkFlag(kw['target'], set_soname)] +
-            self.compiler.linker_flags +
-            self.linker_flags
-        )
 
 class CopyFile(Command):
 
@@ -111,62 +19,57 @@ class CopyFile(Command):
         return ['cp', self.dependencies, kw['target']]
 
 class Library:
-    def __init__(self, names, directories=[]):
+    def __init__(self, names, include_directories=[], directories=[]):
         if not isinstance(names, list):
             names = [names]
         self.names = names
         self.directories = directories
+        self.include_directories = include_directories
 
 
 from tupcfg import tools
 
-class Compiler:
-
-    def __init__(self, name, project, build,
-                 flags=[],
-                 linker_flags=[],
-                 include_directories=[]):
-        self.project = project
-        self.build = build
-        self.linker_flags = linker_flags
-        self.binary = tools.find_binary(name)
-        self.flags = flags
-        self.include_directories = include_directories
-
-    def link_library(self, name, sources, **kw):
-        sources_ = (Source(src) for src in sources)
-        objects = (Target(src.filename + '.o', BuildObject(self, src)) for src in sources_)
-        return self.build.add_target(Target(name, LinkLibrary(self, list(objects), **kw)))
-
-
-    def link_executable(self, name, sources, **kw):
-        sources_ = (Source(src) for src in sources)
-        objects = (Target(src.filename + '.o', BuildObject(self, src)) for src in sources_)
-        return self.build.add_target(Target(name, LinkExecutable(self, list(objects), **kw)))
-
-
-
 def configure(project, build):
-    from tupcfg.tools import glob, status
+    from tupcfg.tools import glob, status, cleanabspath
     from os.path import join
     status("Configuring project", project.env.NAME, 'in', build.directory, '(%s)' % project.env.BUILD_TYPE)
+    from tupcfg.lang.cxx import gcc
 
-    compiler = Compiler("g++-4.7", project, build,
-                        flags=['-fPIC', '-std=c++11', '-x', 'c++',],
-                        linker_flags=['-L/home/hotgloupi/local/lib'],
-                        include_directories=[
-                            '/home/hotgloupi/local/include',
-                            '/usr/include/python3.2mu',
-                            '/usr/include/freetype2/',
-                        ])
+    prefixes = project.env.get('PREFIXES', [])
+    if isinstance(prefixes, str):
+        prefixes = prefixes.split(':')
+    project.env.project_set('PREFIXES', prefixes)
+
+    lib_dirs = list(join(p, 'lib') for p in prefixes)
+    include_dirs = list(join(p, 'include') for p in prefixes)
+
+    compiler = gcc.Compiler(
+        project, build,
+        position_independent_code = True,
+        standard = 'c++11',
+        library_directories = lib_dirs,
+        include_directories = include_dirs + [cleanabspath(join(project.root_dir, 'src'))],
+    )
+    status("CXX compiler is", compiler.binary)
+
+    #compiler = Compiler("g++-4.7", project, build,
+    #                    flags=['-fPIC', '-std=c++11', '-x', 'c++',],
+    #                    linker_flags=['-L/home/hotgloupi/local/lib'],
+    #                    include_directories=[
+    #                        '/home/hotgloupi/local/include',
+    #                        '/usr/include/python3.2mu',
+    #                        '/usr/include/freetype2/',
+    #                    ])
 
     boost_libraries = list(
         Library('boost_' + s) for s in ['python', 'filesystem', 'signals', 'system']
     )
     python_libraries = [
-        Library('python3.2mu'),
+        Library('python3.2mu', include_directories=['/usr/include/python3.2mu'])
     ]
-    graphic_libraries = list(Library(s) for s in ['SDL', 'SDL_image', 'GL', 'GLU', 'GLEW', 'freetype'])
+    graphic_libraries = list(Library(s) for s in ['SDL', 'SDL_image', 'GL', 'GLU', 'GLEW']) + [
+        Library('freetype', include_directories=['/usr/include/freetype2'])
+    ]
 
     libetc = compiler.link_library(
         'release/lib/libetc.so',
@@ -176,7 +79,7 @@ def configure(project, build):
     libcube = compiler.link_library(
         'release/lib/libcube.so',
         glob("src/cube/*.cpp", recursive=True),
-        link_libraries=[libetc] + graphic_libraries,
+        libraries=[libetc] + graphic_libraries + python_libraries,
     )
 
 
@@ -185,7 +88,7 @@ def configure(project, build):
         libcube_bindings.append(compiler.link_library(
             join("release/lib/python/cube", binding[9:-5] + '.so'),
             [binding],
-            link_libraries=[libetc, libcube] + boost_libraries + python_libraries,
+            libraries=[libetc, libcube] + boost_libraries + python_libraries,
         ))
 
     build.add_targets(
@@ -212,7 +115,7 @@ def configure(project, build):
     infinit_cube = compiler.link_executable(
         "release/bin/8cube",
         glob("src/app/*.cpp", recursive=True),
-        link_libraries=[libetc, libcube] + boost_libraries + python_libraries,
+        libraries=[libetc, libcube] + boost_libraries + python_libraries,
     )
 
     #build.dump(project)
