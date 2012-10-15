ec0f36aae016a9e3b2409d53b8eb673121427a56
Raphael Londeix <raphael.londeix@gmail.com>
2012-10-15 03:20:37 +0200

    fix: static linking in boost python works only with a special define

--- src/wrappers/boost/python.hpp
+++ src/wrappers/boost/python.hpp
@@ -1,20 +1,22 @@
 #ifndef  _PYTHON_BOOST_HPP
 # define _PYTHON_BOOST_HPP
 
-# include <boost/python.hpp>
-# include <cstddef> // std:size_t
+#define BOOST_PYTHON_STATIC_LIB 1
+
+# if (defined(__MINGW32__) || defined(mingw32) || defined(__MINGW64__) || defined(mingw64)) && (__GNUC__ == 4)
 
 // workaround a mingw bug
 // http://lists.boost.org/boost-users/2010/12/65159.php
-# if (defined(__MINGW32__) || defined(__MINGW64__)) && (__GNUC__ == 4)
 #  include <cmath>
-# endif
 
 // workaround a mingw bug
 // http://sourceforge.net/tracker/index.php?func=detail&aid=2373234&group_id=2435&atid=102435
-# if (defined(__MINGW32__) || defined(__MINGW64__)) && (__GNUC__ == 4)
+#  include <cstddef> // std:size_t
 int swprintf (wchar_t *, size_t, const wchar_t *, ...);
+
 # endif
+
+# include <boost/python.hpp>
 
 namespace boost { namespace python {
 
