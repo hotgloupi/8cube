2c8eca9b3a7633681b9b1b25b75f2b33b5d95155
Raphaël Londeix <raphael.londeix@gmail.com>
2012-11-05 00:10:18 +0100

    fix: check all hold key

--- share/games/test/player.py
+++ share/games/test/player.py
@@ -23,12 +23,12 @@ class Player(core.Player):
         move = gl.Vector3f()
         if self.inputs.move_forward.held:
             move += self.camera.front
-        elif self.inputs.move_backward.held:
+        if self.inputs.move_backward.held:
             move -= self.camera.front
 
         if self.inputs.strafe_right.held:
             move += self.camera.right
-        elif self.inputs.strafe_left.held:
+        if self.inputs.strafe_left.held:
             move -= self.camera.right
 
         if move.x or move.y or move.z:
