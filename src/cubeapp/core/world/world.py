4d866f4595c624207ab4368a1d59ca903f24f5a0
Raphaël Londeix <raphael.londeix@gmail.com>
2013-01-08 08:11:19 +0100

    display on tree visit result

--- src/cubeapp/core/world/world.py
+++ src/cubeapp/core/world/world.py
@@ -4,6 +4,7 @@ from cube import gl
 
 from .storage import Storage
 from .generator import Generator
+from . import tree
 from .chunk import Chunk
 
 class World:
@@ -13,12 +14,24 @@ class World:
         self.__storage = storage
         self.__generator = generator
         self.__renderer = renderer
-        self.__xxx_chunks = {}
+        self.__tree = tree.Tree()
         Chunk.prepare(renderer)
 
     def update(self, delta, player):
         pos = self._chunk_coord(player.position)
-        self.__xxx_chunks[(pos.x, pos.y, pos.z)] = self.get_chunk(pos)
+        self.__player_pos = gl.Vector3il(pos.x, pos.y, pos.z)
+        self.__nodes_to_render = []
+        self.__tree.visit_nodes(self.__on_tree_node)
+        #print("Found", len(self.__nodes_to_render), "nodes")
+
+    def __on_tree_node(self, node):
+        if node.level == 0:
+            self.__nodes_to_render.append(
+                (node.origin, self.get_chunk(node.origin))
+            )
+        if node.contains(self.__player_pos):
+            return tree.VisitAction.continue_
+        return tree.VisitAction.stop_and_clean
 
     def get_chunk(self, pos):
         chunk = self.__storage.get_chunk(pos)
@@ -28,16 +41,14 @@ class World:
         return chunk
 
     def render(self, painter):
-        if not self.__xxx_chunks:
-            return
         with painter.bind([Chunk.sp, Chunk.vb]):
-            for pos, chunk in self.__xxx_chunks.items():
-                chunk.render(gl.Vector3i(*pos), painter)
+            for pos, chunk in self.__nodes_to_render:
+                chunk.render(pos, painter)
 
     @staticmethod
     def _chunk_coord(v):
         rounded = v / 16.0
-        pos = gl.Vector3i(rounded.x, rounded.y, rounded.z)
+        pos = gl.Vector3il(rounded.x, rounded.y, rounded.z)
         if v.x < 0:
             pos.x -= 1
         if v.y < 0:
