3d41d5b9df18ad95838d2eff14add69fe308aa59
Raphaël Londeix <raphael.londeix@gmail.com>
2013-02-24 18:33:52 +0100

    add: test sphere intersect

--- tests/cube/gl/frustum.py
+++ tests/cube/gl/frustum.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-from cube.gl import frustum, vec3il, vec3f
+from cube.gl import frustum, vec3il, vec3f, Sphereil
 
 fov = 45
 ratio = 640 / 480
@@ -19,4 +19,21 @@ for p in frustum.PlanePosition.values.values():
     print(p, f.plane(p).distance(pos))
 
 print(f.contains(pos))
-print(pos, f.contains(pos))
+assert f.contains(pos)
+
+
+s = Sphereil(pos, 12)
+assert f.intersect(s)
+
+pos = vec3il(10,15,11)
+s = Sphereil(pos, 12)
+assert f.intersect(s)
+
+
+pos = vec3il(10,12,-289)
+s = Sphereil(pos, 1)
+assert f.intersect(s)
+
+pos = vec3il(10,12,-290)
+s = Sphereil(pos, 1)
+assert not f.intersect(s)
