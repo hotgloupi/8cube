e78cfe0ba2d7f5813ceb07367464982f2f4b968f
Raphaël Londeix <raphael.londeix@gmail.com>
2013-03-12 09:33:33 +0100

    use angle type for frustum fov, use largest face for plane determination

--- src/cube/gl/camera.cpp
+++ src/cube/gl/camera.cpp
@@ -1,4 +1,6 @@
 #include "camera.hpp"
+#include "matrix.hpp"
+#include <etc/print.hpp>
 
 namespace cube { namespace gl { namespace camera {
 
@@ -7,29 +9,24 @@ namespace cube { namespace gl { namespace camera {
 	               units::Angle const vertical_angle)
 	{
 		_hrot += horizontal_angle;
+		while (_hrot < units::rad(0))
+			_hrot += units::rad(3.14159265*2);
+		while (_hrot > units::rad(3.14159265*2))
+			_hrot -= units::rad(3.14159265*2);
+
 		_vrot += vertical_angle;
 		if (_vrot > units::rad(3.14159 / 2))
 			_vrot = units::rad(3.14159 / 2 - 0.01);
 		if (_vrot < units::rad(-3.14159 / 2))
 			_vrot = units::rad(-3.14159 / 2 + 0.01);
-		_front = vector::normalize(
-			vector::rotate(
-				vector::rotate(
-					vec3{0,0,1},
-					units::deg_value(_vrot),
-					vec3{1,0,0}
-				),
-				units::deg_value(_hrot),
-				vec3{0,1,0}
-			)
-		);
-		_up = vector::normalize(
-			vector::rotate(
-				vec3{0,1,0},
-				units::deg_value(_vrot),
-				vec3{1,0,0}
-			)
-		);
+
+		_front = vector::rotate_x(vec3{0,0,-1}, units::deg_value(_vrot));
+		_front = vector::rotate_y(_front, units::deg_value(_hrot));
+		_up = vector::rotate_x(vec3{0,1,0}, units::deg_value(_vrot));
+		_up = vector::rotate_y(_up, units::deg_value(_hrot));
+
+		double dot = vector::dot(_up, _front);
+		assert(dot < 1e-7);
 		return *this;
 	}
 
