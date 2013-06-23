4a51e8529d682fe64e4ef325757cd3d719584c60
Raphaël Londeix <raphael.londeix@gmail.com>
2013-06-23 19:26:24 +0200

    Bind type attribute

--- src/cube/gl/renderer_bindings/shader_generator.cpp
+++ src/cube/gl/renderer_bindings/shader_generator.cpp
@@ -13,12 +13,14 @@ namespace cube { namespace gl { namespace renderer_bindings {
 	public:
 		bool is_applicable(ShaderType type) const override
 		{
+			throw std::runtime_error("Not implemented");
 			return true;
 		}
 
 		std::string source(ShaderGeneratorProxy const& proxy,
 		                   std::string const& name) const override
 		{
+			throw std::runtime_error("Not implemented");
 			return "";
 		}
 	};
@@ -35,11 +37,17 @@ namespace cube { namespace gl { namespace renderer_bindings {
 		py::class_<
 			ShaderGeneratorProxy
 		>("ShaderGeneratorProxy", py::no_init)
+			.def_readonly("type", &ShaderGeneratorProxy::type)
+			//.def_readonly("generator", &ShaderGeneratorProxy::generator)
+			//.def_readonly("renderer", &ShaderGeneratorProxy::renderer)
+			//.def_readonly("parameters", &ShaderGeneratorProxy::parameters)
+			//.def_readonly("inputs", &ShaderGeneratorProxy::inputs)
+			//.def_readonly("outputs", &ShaderGeneratorProxy::outputs)
 			.def("source", &ShaderGeneratorProxy::source)
 			.def("shader", &ShaderGeneratorProxy::shader)
 			.def("input", &ShaderGeneratorProxy::in, py::return_internal_reference<>())
 			.def("output", &ShaderGeneratorProxy::out, py::return_internal_reference<>())
-			.def("parameter", &ShaderGeneratorProxy::shader)
+			.def("parameter", &ShaderGeneratorProxy::parameter, py::return_internal_reference<>())
 		;
 
 		py::class_<
@@ -47,6 +55,7 @@ namespace cube { namespace gl { namespace renderer_bindings {
 			boost::noncopyable
 		>("ShaderRoutine")
 			.def("source", &ShaderRoutine::source)
+			.def("is_applicable", &ShaderRoutine::is_applicable)
 		;
 	}
 
