9899d9ac544665674506a8601434b98afca43262
Raphaël Londeix <raphael.londeix@infinit.io>
2013-07-05 21:33:17 +0200

    Add wrappers meant for precompiled headers

---
+++ src/wrappers/boost/signals2.hpp
@@ -0,0 +1,6 @@
+#ifndef  WRAPPERS_BOOST_SIGNALS2_HPP
+# define WRAPPERS_BOOST_SIGNALS2_HPP
+
+# include <boost/signals2.hpp>
+
+#endif
---
+++ src/wrappers/stl.hpp
@@ -0,0 +1,13 @@
+#ifndef  WRAPPER_STL_HPP
+# define WRAPPER_STL_HPP
+
+// Precompiled header of most common stl classes
+
+# include <functional>
+# include <memory>
+# include <string>
+# include <unordered_map>
+# include <unordered_set>
+# include <vector>
+
+#endif
