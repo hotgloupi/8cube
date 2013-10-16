bf8cec4f13a8431938ef832b70b2556cbfe42d12
Raphaël Londeix <raphael.londeix@infinit.io>
2013-10-16 09:33:10 +0200

    Add keyboard key in bindings dictionary.

--- share/games/test/bindings.py
+++ share/games/test/bindings.py
@@ -4,10 +4,11 @@ from cube.system.inputs import KeySym as sym
 
 BINDINGS = {
 
-    'move_forward': sym.up,
-    'move_backward': sym.down,
-    'strafe_left': sym.left,
-    'strafe_right': sym.right,
-
+    'keyboard': {
+        'move_forward': sym.up,
+        'move_backward': sym.down,
+        'strafe_left': sym.left,
+        'strafe_right': sym.right,
+    },
 }
 
