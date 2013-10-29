3c82551b20435837f11f150203304f21a695ef42
Raphaël Londeix <raphael.londeix@infinit.io>
2013-10-29 11:30:49 +0100

    Include Window shell api, and force cleaner windows.h.

--- src/wrappers/windows.hpp
+++ src/wrappers/windows.hpp
@@ -2,9 +2,11 @@
 # define LIB_WRAPPERS_WINDOWS_HPP
 
 # ifdef _WIN32
+#  define WIN32_LEAN_AND_MEAN
 #  include <Windows.h>
 #  include <Winbase.h>
 #  include <Shlobj.h>
+#  include <Shlwapi.h>
 #  undef DELETE
 #  undef far
 #  undef near
