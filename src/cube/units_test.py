7162b10e5ea0a33d00415684fc7dc955c944b0f1
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-07 23:25:50 +0100

    cube.units: Add tests.

---
+++ src/cube/units_test.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf8 -*-
+
+from unittest import TestCase
+
+from .units import Angle, rad, deg, rad_value, deg_value
+
+class _(TestCase):
+
+    def test_angle(self):
+        a = Angle()
+        self.assertEqual(rad_value(a), 0)
+        self.assertEqual(deg_value(a), 0)
