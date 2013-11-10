de593ca812214503a8499573c35b718a25f83f0e
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-10 15:36:51 +0100

    cube.exception: Add tests.

---
+++ src/cube/exception_test.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+
+from . import exception
+
+from unittest import TestCase
+
+class _(TestCase):
+
+    def test_raise_from_cpp(self):
+        try:
+            exception.test_throw("TestException")
+        except exception.Exception as e:
+            self.assertEqual(str(e), "TestException")
+        else:
+            self.fail("Coudn't catch the right exception type")
+
+    def test_raise_from_python(self):
+        try:
+            raise exception.Exception("TestException")
+        except exception.Exception as e:
+            self.assertEqual(str(e), "TestException")
+        else:
+            self.fail("Couldn't catch the right exception type")
+
+    def test_raise_from_cpp_in_a_python_callback(self):
+        def cb():
+            raise exception.Exception("TestException")
+        try:
+            exception.test_cb(cb)
+        except exception.Exception as e:
+            self.assertEqual(str(e), "TestException")
+        else:
+            self.fail("Couldn't catch the right exception type")
