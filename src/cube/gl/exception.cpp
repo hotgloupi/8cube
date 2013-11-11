523f892f4a75ad2c9c242f3c02f4371afcb53de8
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-11 18:25:40 +0100

    cube.exception: Do not log on creation.

--- src/cube/gl/exception.cpp
+++ src/cube/gl/exception.cpp
@@ -10,13 +10,13 @@ namespace cube { namespace gl { namespace exception {
 	Exception::Exception(std::string const& msg)
 		: cube::exception::Exception(msg)
 	{
-		auto error = ETC_LOG.error("Exception thrown:", msg);
-		if (auto bt = this->backtrace())
-		{
-			etc::size_type i = 0;
-			for (auto const& frame: *bt)
-				error("\t", i++, frame);
-		}
+		//auto error = ETC_LOG.error("Exception thrown:", msg);
+		//if (auto bt = this->backtrace())
+		//{
+		//	etc::size_type i = 0;
+		//	for (auto const& frame: *bt)
+		//		error("\t", i++, frame);
+		//}
 	}
 
 }}}
