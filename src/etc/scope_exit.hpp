cda5ef320f91fcdb5ac208e853a5d41452f34eb2
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-14 22:45:53 +0100

    etc.scope_exit: Add simple (unsafe) implem of a scope exit guard.

---
+++ src/etc/scope_exit.hpp
@@ -0,0 +1,81 @@
+#ifndef  ETC_SCOPE_EXIT_HPP
+# define ETC_SCOPE_EXIT_HPP
+
+# include "meta/clean_type.hpp"
+
+# include <cstdlib>
+
+namespace etc {
+
+	namespace detail {
+
+		template<typename Callback>
+		struct scope_exit
+		{
+		private:
+			Callback _callback;
+			bool     _aborted;
+
+		public:
+			template<typename T>
+			inline
+			scope_exit(T&& cb)
+				: _callback{cb}
+				, _aborted{false}
+			{}
+
+			/// Move operation
+			// XXX Should be enabled only if the callback move is nothrow
+			scope_exit(scope_exit const& other) = delete;
+			inline
+			scope_exit(scope_exit&& other)
+				: _callback{std::move(other._callback)}
+				, _aborted{other._aborted}
+			{ other._aborted = true; }
+
+			scope_exit operator =(scope_exit const& other) = delete;
+			scope_exit operator =(scope_exit&& other) = delete;
+
+			inline
+			~scope_exit()
+			{
+				if (not _aborted)
+				{
+					try { _callback(); }
+					catch (...) { std::abort(); }
+				}
+			}
+
+			void dismiss() { _aborted = true; }
+		};
+
+		struct scope_exit_helper
+		{
+			template<typename Lambda>
+			inline
+			scope_exit<typename meta::clean_type<Lambda>::type>
+			operator +(Lambda&& lambda)
+			{
+				return scope_exit<typename meta::clean_type<Lambda>::type>{
+					std::forward<Lambda>(lambda)
+				};
+			}
+		};
+
+	}
+
+	template<typename Lambda>
+	inline
+	detail::scope_exit<typename meta::clean_type<Lambda>::type>
+	scope_exit(Lambda&& lambda)
+	{
+		return detail::scope_exit<typename meta::clean_type<Lambda>::type>{
+			std::forward<Lambda>(lambda)
+		};
+	}
+
+#define ETC_SCOPE_EXIT \
+	auto dummy ## __LINE__ = ::etc::detail::scope_exit_helper{} + [&]
+}
+
+#endif
