a5164278410ed7a57850a2b39b8c36d91ffce395
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-17 20:49:49 +0100

    etc:  graph container draft.

---
+++ src/etc/graph.hpp
@@ -0,0 +1,69 @@
+#ifndef ETC_GRAPH_HPP
+# define ETC_GRAPH_HPP
+
+# include <cassert>
+# include <memory>
+# include <set>
+
+namespace etc {
+
+	template<typename Value>
+	struct Node
+		: public std::enable_shared_from_this<Node<Value>>
+	{
+		typedef Value value_type;
+		typedef std::shared_ptr<value_type> shared_node_type;
+		typedef std::weak_ptr<value_type> weak_node_type;
+
+		std::set<weak_node_type> parents;
+		std::set<shared_node_type> children;
+
+		inline
+		void add_child(shared_node_type& node) noexcept
+		{
+			assert(node != nullptr);
+			auto res = this->children.insert(node);
+			if (res.second == false)
+				return;
+			node->add_parent(this->shared_from_this());
+		}
+
+		inline
+		void add_parent(shared_node_type& node) noexcept
+		{
+			assert(node != nullptr);
+			auto res = this->parents.insert(node);
+			if (res.second == false)
+				return;
+			node->add_child(this->shared_from_this());
+		}
+
+		inline
+		void remove_child(shared_node_type& node) noexcept
+		{
+			assert(node != nullptr);
+			if (this->children.erase(node) == 0)
+				return;
+			node->remove_parent(this->shared_from_this());
+		}
+
+		inline
+		void remove_parent(shared_node_type& node) noexcept
+		{
+			assert(node != nullptr);
+			if (this->parents.erase(node) == 0)
+				return;
+			node->remove_child(this->shared_from_this());
+		}
+	};
+
+	template<typename Node>
+	struct Graph
+	{
+		typedef Node node_type;
+		typedef typename Node::value_type value_type;
+	};
+
+}
+
+#endif
