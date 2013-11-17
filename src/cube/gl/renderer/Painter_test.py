d7e435af995cb958be609c9f9f9613e8288aa0bc
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-17 20:36:31 +0100

    cube.gl.renderer: Add transformation test in Painter tests.

--- src/cube/gl/renderer/Painter_test.py
+++ src/cube/gl/renderer/Painter_test.py
@@ -78,59 +78,77 @@ class PainterSetup:
             )
         )
 
-def painter_test(func):
-    def wrapper(self):
-        fname = list(filter(
-            lambda e: bool(e),
-            self.__class__.__module__.lower().replace('_test', '').split('.')
-        ))[-1]
-        cls = self.__class__.__name__.lower().replace('_', '')
-        if cls: fname += '_' + cls
-        fname += '_' + func.__name__
-        source_dir = path.dirname(inspect.getfile(self.__class__))
-        thruth = path.join(source_dir, "%s.bmp" % fname)
-        img = path.join(source_dir, "%s-tmp.bmp" % fname)
-        first_time = not path.exists(thruth)
 
-        if first_time:
-            img = thruth
+def painter_test(mode):
+    def _painter_test(func):
+        def wrapper(self):
+            fname = list(filter(
+                lambda e: bool(e),
+                self.__class__.__module__.lower().replace('_test', '').split('.')
+            ))[-1]
+            cls = self.__class__.__name__.lower().replace('_', '')
+            if cls: fname += '_' + cls
+            fname += '_' + func.__name__
+            source_dir = path.dirname(inspect.getfile(self.__class__))
+            thruth = path.join(source_dir, "%s.bmp" % fname)
+            img = path.join(source_dir, "%s-tmp.bmp" % fname)
+            first_time = not path.exists(thruth)
 
-        with self.renderer.begin(mode_2d) as painter:
-            with painter.bind([self.target]):
-                self.renderer.clear(1)
-                func(self, painter)
-            self.target.save(img)
-
-        if first_time:
-            self.window.show()
-            running = [1]
-            self.renderer.clear(1)
-            slot = self.window.inputs.on_quit.connect(lambda: running.pop())
-            self.window.title = fname
-            while running:
-                self.window.swap_buffers()
-                with self.renderer.begin(mode_2d) as painter:
+            with self.renderer.begin(mode) as painter:
+                with painter.bind([self.target]):
                     self.renderer.clear(1)
                     func(self, painter)
-                self.window.poll()
-            slot.disconnect()
-            print("Compare the previous output with the file %s" % img)
-            sys.exit(0)
+                self.target.save(img)
 
-        with open(thruth, 'rb') as f1:
-            with open(img, 'rb') as f2:
-                self.assertTrue(f1.read() == f2.read(),
-                                "%s and %s are not the same" % (thruth, img))
-        os.unlink(img)
+            if first_time:
+                print("\n\n###############################################")
+                print("##### NEW PAINTER TEST NEEDS MANUAL VALIDATION:\n")
+                print("Check the output and copy the temporary file: \n\t>> %s" % img)
+                print("to: \n\t>> %s" % thruth)
+                print("\n###############################################\n")
+                self.window.show()
+                running = [1]
+                self.renderer.clear(1)
+                slot = self.window.inputs.on_quit.connect(lambda: running.pop())
+                self.window.title = fname
+                while running:
+                    self.window.swap_buffers()
+                    with self.renderer.begin(mode) as painter:
+                        self.renderer.clear(1)
+                        func(self, painter)
+                    self.window.poll()
+                slot.disconnect()
+                print("\n\n###############################################")
+                print("##### NEW PAINTER TEST NEEDS MANUAL VALIDATION:\n")
+                print("Check the output and copy the temporary file: \n\t>> %s" % img)
+                print("to: \n\t>> %s" % thruth)
+                print("\n###############################################\n")
+                sys.exit(0)
 
-    wrapper.__name__ = func.__name__
-    return wrapper
+            with open(thruth, 'rb') as f1:
+                with open(img, 'rb') as f2:
+                    self.assertTrue(f1.read() == f2.read(),
+                                    "%s and %s are not the same" % (thruth, img))
+            os.unlink(img)
+
+        wrapper.__name__ = func.__name__
+        return wrapper
+    return _painter_test
 
 class _(PainterSetup, TestCase):
 
-    @painter_test
+    @painter_test(mode_2d)
     def test_simple(self, painter):
         with painter.bind([self.shader, self.vb]):
             self.shader['cube_MVP'] = painter.state.mvp
             painter.draw_elements(DrawMode.quads, self.indices, 0, 4)
 
+
+    @painter_test(mode_2d)
+    def test_transform(self, painter):
+        with painter.bind([self.shader, self.vb]):
+            painter.state.model = matrix.scale(painter.state.model, 0.5, 0.5, 0.5)
+            painter.state.model = matrix.translate(
+                painter.state.model, 100, 75, 0)
+            self.shader['cube_MVP'] = painter.state.mvp
+            painter.draw_elements(DrawMode.quads, self.indices, 0, 4)
