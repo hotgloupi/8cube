6750edcfd3abce3d0f5f935f1a5c939b41c6370e
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-17 20:47:56 +0100

    cube.gl: Add surface module.

---
+++ src/cube/gl/surface.cpp
@@ -0,0 +1,87 @@
+#include "surface.hpp"
+#include "exception.hpp"
+
+#include <etc/log.hpp>
+
+#include <SDL_image.h>
+
+namespace cube { namespace gl { namespace surface {
+
+	using exception::Exception;
+
+	ETC_LOG_COMPONENT("cube.gl.surface.Surface");
+
+	struct Surface::Impl
+	{
+		SDL_Surface* surface;
+
+		Impl()
+			: surface{nullptr}
+		{}
+
+		~Impl()
+		{
+			if (this->surface != nullptr)
+				SDL_FreeSurface(this->surface);
+		}
+	};
+
+	Surface::Surface(std::string const& path)
+		: _this{new Impl}
+	{
+		ETC_TRACE_CTOR("from", path);
+		_this->surface = ::IMG_Load(path.c_str());
+		if (_this->surface == nullptr)
+			throw Exception{"Couldn't load image '" + path + "'"};
+	}
+	Surface::Surface(PixelFormat const format,
+	                 unsigned int width,
+	                 unsigned int height,
+	                 PixelFormat const data_format,
+	                 ContentPacking const data_packing,
+	                 void const* data)
+		: _this{new Impl}
+	{
+		if (data == nullptr)
+		{
+			auto mask = renderer::pixel_mask(format);
+			int depth = renderer::pixel_depth(format);
+			_this->surface = SDL_CreateRGBSurface(
+				0, width, height, depth,
+				mask.red, mask.green, mask.blue, mask.alpha
+			);
+		}
+		else
+		{
+			if (format != data_format)
+				throw Exception{"Conversion not supported"};
+			int depth = renderer::pixel_depth(data_format);
+			auto mask = renderer::pixel_mask(data_format);
+			int pitch = depth / 8 * width;
+			_this->surface = SDL_CreateRGBSurfaceFrom(
+				(void*)data, width, height, depth, pitch,
+				mask.red, mask.green, mask.blue, mask.alpha
+			);
+		}
+		if (_this->surface == nullptr)
+			throw Exception{
+				"Couldn't create the surface: " + std::string(SDL_GetError())
+			};
+	}
+
+	Surface::~Surface()
+	{ ETC_TRACE_DTOR(); }
+
+	etc::size_type Surface::bytes_per_pixel() const noexcept
+	{ return _this->surface->format->BytesPerPixel; }
+
+	etc::size_type Surface::width() const noexcept
+	{ return _this->surface->w; }
+
+	etc::size_type Surface::height() const noexcept
+	{ return _this->surface->h; }
+
+	void const* Surface::pixels() const noexcept
+	{ return _this->surface->pixels; }
+
+}}}
