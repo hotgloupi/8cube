239eb93d090174bfa0948c788740408ee054d5e4
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-29 03:39:57 +0100

    cube.gl.material: Generate a bindable object.

--- src/cube/gl/material.cpp
+++ src/cube/gl/material.cpp
@@ -1,5 +1,13 @@
 #include "material.hpp"
 
+#include <cube/gl/renderer/Renderer.hpp>
+#include <cube/gl/renderer/Bindable.hpp>
+#include <cube/gl/renderer/ShaderGenerator.hpp>
+#include <cube/gl/renderer/ShaderProgram.hpp>
+#include <cube/gl/renderer.hpp>
+#include <cube/system/window.hpp>
+
+#include <etc/stack_ptr.hpp>
 #include <etc/test.hpp>
 
 namespace cube { namespace gl { namespace material {
@@ -12,12 +20,127 @@ namespace cube { namespace gl { namespace material {
 
 	namespace {
 
+		struct Bindable
+			: public renderer::Bindable
+		{
+			Material const&        material;
+			renderer::ShaderProgramPtr shader_program;
+			etc::stack_ptr<Guard>  bind_guard;
+
+			Bindable(Material const& material,
+			         renderer::ShaderProgramPtr shader_program)
+				: material(material)
+				, shader_program{std::move(shader_program)}
+				, bind_guard{etc::stack_ptr_no_init}
+			{}
+
+			void _bind(renderer::State const& s) override
+			{
+				bind_guard.reset(*shader_program, s);
+				shader_program->parameter("cube_MVP") = s.mvp();
+			}
+
+			void _unbind() noexcept override
+			{
+				bind_guard.clear();
+			}
+		};
+
+		struct Main
+			: public renderer::ShaderRoutine
+		{
+			using ShaderRoutine::ShaderRoutine;
+			std::string
+			glsl_source() const override
+			{
+				std::string res;
+				if (proxy.type == renderer::ShaderType::vertex)
+				{
+					res += "\tgl_Position = cube_MVP * vec4(cube_Vertex, 1);\n";
+					res += "\tcube_Color = vec4(cube_VertexColor.xyz, 1);\n";
+				}
+				else if (proxy.type == renderer::ShaderType::fragment)
+				{
+					res += "\tcube_FragColor = cube_Color;\n";
+				}
+
+				return res;
+			}
+		};
+
+	} // !anonymous
+
+	renderer::BindablePtr
+	Material::bindable(renderer::Renderer& renderer) const
+	{
+		using renderer::ShaderType;
+		using renderer::ShaderParameterType;
+		using renderer::ContentKind;
+
+		auto vs = renderer.generate_shader(ShaderType::vertex);
+		vs.parameter(ShaderParameterType::float_, "cube_Shininess")
+			.parameter(ShaderParameterType::float_, "cube_Opacity")
+			.parameter(ShaderParameterType::vec3, "cube_Diffuse")
+			.parameter(ShaderParameterType::vec3, "cube_Ambient")
+			.parameter(ShaderParameterType::vec3, "cube_Specular")
+			.parameter(ShaderParameterType::mat4, "cube_MVP")
+			.input(
+				ShaderParameterType::vec3,
+				"cube_Vertex",
+				ContentKind::vertex
+			)
+			.input(
+				ShaderParameterType::vec3,
+				"cube_VertexColor",
+				ContentKind::color
+			)
+			//.output(
+			//	ShaderParameterType::vec3,
+			//	"cube_Position",
+			//	ContentKind::vertex
+			//)
+			.output(
+				ShaderParameterType::vec4,
+				"cube_Color",
+				ContentKind::color
+			)
+			.routine<Main>("main")
+		;
+
+		auto fs = renderer.generate_shader(ShaderType::fragment);
+		fs.input(
+				ShaderParameterType::vec4,
+				"cube_Color",
+				ContentKind::color
+			)
+			.output(
+				ShaderParameterType::vec4,
+				"cube_FragColor",
+				ContentKind::color
+			)
+			.routine<Main>("main")
+		;
+
+		//etc::print("vertex shader:", vs.source());
+		//etc::print("fragment shader:", fs.source());
+
+		auto shader_program = renderer.new_shader_program(
+			vs.shader(),
+			fs.shader()
+		);
+		return renderer::BindablePtr{
+			new Bindable{*this, std::move(shader_program)}
+		};
+	}
+
+	namespace {
+
 		ETC_TEST_CASE(material_default_values)
 		{
 			Material m;
 			Material::color_type black;
 
-			ETC_ENFORCE_EQ(m.name(), "unnamed");
+			ETC_ENFORCE_EQ(m.name(), "unamed");
 			ETC_ENFORCE_EQ(m.diffuse(), black);
 			ETC_ENFORCE_EQ(m.ambient(), black);
 			ETC_ENFORCE_EQ(m.specular(), black);
--- src/cube/gl/material.hpp
+++ src/cube/gl/material.hpp
@@ -4,6 +4,8 @@
 # include "fwd.hpp"
 # include "color.hpp"
 
+# include <cube/gl/renderer/fwd.hpp>
+
 # include <string>
 # include <vector>
 
@@ -18,7 +20,7 @@ namespace cube { namespace gl { namespace material {
 		plane,
 	};
 
-	enum class TextureOperation
+	enum class StackOperation
 	{
 		multiply,
 		add,
@@ -66,13 +68,13 @@ namespace cube { namespace gl { namespace material {
 			std::string path;
 			TextureType type;
 			TextureMapping mapping;
-			TextureOperation operation;
+			StackOperation operation;
 			TextureMapMode map_mode;
 			float blend;
 			Texture(std::string path,
 			        TextureType type,
 			        TextureMapping mapping,
-			        TextureOperation operation,
+			        StackOperation operation,
 			        TextureMapMode map_mode,
 			        float blend)
 				: path{std::move(path)}
@@ -84,6 +86,7 @@ namespace cube { namespace gl { namespace material {
 			{}
 		};
 		typedef std::vector<Texture> TextureStack;
+		typedef std::vector<StackOperation> ColorStack;
 
 	private:
 		std::string  _name;
@@ -93,52 +96,87 @@ namespace cube { namespace gl { namespace material {
 		float        _shininess;
 		float        _opacity;
 		TextureStack _textures;
+		ColorStack   _colors;
 
 	public:
 		explicit
-		Material(std::string name = "unnamed");
+		Material(std::string name = "unamed");
 
-	/// Setter/getter for name.
-	public:
+		/// Name.
 		inline std::string const& name() const noexcept { return _name; }
+
+		/// Set name.
 		inline void name(std::string value)
 			noexcept(std::is_nothrow_move_assignable<std::string>())
 		{ _name = std::move(value); }
 
-	/// Setter/getter for ambient color.
-	public:
+		/// Ambient color.
 		inline color_type const& ambient() const noexcept { return _ambient; }
+
+		/// Set ambient color.
 		inline void ambient(color_type color) noexcept
 		{ _ambient = std::move(color); }
 
-	/// Setter/getter for diffuse color.
-	public:
+		/// Diffuse color.
 		inline color_type const& diffuse() const noexcept { return _diffuse; }
+
+		/// Set diffuse color.
 		inline void diffuse(color_type color) noexcept
 		{ _diffuse = std::move(color); }
 
+		/// Specular color.
+		inline color_type const& specular() const noexcept
+		{ return _specular; }
 
-	/// Setter/getter for specular color.
-	public:
-		inline color_type const& specular() const noexcept { return _specular; }
+		/// Set specular color.
 		inline void specular(color_type color) noexcept
 		{ _specular = std::move(color); }
 
-	/// Setter/getter for shininess.
-	public:
+		/// Shininess.
 		inline float shininess() const noexcept { return _shininess; }
+
+		/// Set shininess.
 		inline void shininess(float value) noexcept { _shininess = value; }
 
-	/// Setter/getter for opacity.
-	public:
+		/// Opacity.
 		inline float opacity() const noexcept { return _opacity; }
+
+		/// Set opacity.
 		inline void opacity(float value) noexcept { _opacity = value; }
 
-	/// Retreive texture stack.
-	public:
+		/// Append a texture channel.
+		inline void add_texture(std::string path,
+		                        TextureType type,
+		                        TextureMapping mapping,
+		                        StackOperation operation,
+		                        TextureMapMode map_mode,
+		                        float blend)
+		{
+			_textures.emplace_back(
+				std::move(path),
+				type,
+				mapping,
+				operation,
+				map_mode,
+				blend
+			);
+		}
+
+		/// Texture channels stack.
 		inline TextureStack const& textures() const noexcept
 		{ return _textures; }
-		inline TextureStack& textures() noexcept { return _textures; }
+
+		/// Append a color channel.
+		inline void add_color(StackOperation op)
+		{ _colors.push_back(op); }
+
+		/// Color channels stack.
+		inline ColorStack const& colors() const noexcept
+		{ return _colors; }
+
+	public:
+		renderer::BindablePtr
+		bindable(renderer::Renderer& renderer) const;
 	};
 
 }}}
