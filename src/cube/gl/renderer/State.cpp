71ea8f30b69a95ab9ea52676fa2783d761a26cb3
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-30 18:31:39 +0100

    cube.gl.renderer: Rework State implem completely.

--- src/cube/gl/renderer/State.cpp
+++ src/cube/gl/renderer/State.cpp
@@ -1,95 +1,154 @@
 #include "State.hpp"
 
 #include "Exception.hpp"
+#include "Light.hpp"
 #include "Painter.hpp"
 
 #include <etc/log.hpp>
 #include <etc/to_string.hpp>
 
+#include <vector>
+
 namespace cube { namespace gl { namespace renderer {
 
 	ETC_LOG_COMPONENT("cube.gl.renderer.State");
 
-	State& State::operator =(State const& other)
+	struct State::Impl
 	{
-		if (this != &other)
+		matrix_type           model;
+		matrix_type           view;
+		matrix_type           projection;
+		matrix_type           mvp;
+		bool                  mvp_dirty;
+		std::vector<LightPtr> lights;
+		Impl() : mvp_dirty{false} {}
+		Impl(Impl const& other)
+			: model{other.model}
+			, view{other.view}
+			, projection{other.projection}
+			, mvp{other.mvp}
+			, mvp_dirty{other.mvp_dirty}
+			, lights{other.lights}
+		{}
+	};
+
+	State::State(Mode const mode)
+		: mode(mode)
+		, _this{new Impl{}}
+	{}
+
+	State::State(State&& other)
+		noexcept(std::is_nothrow_move_constructible<State::Impl>())
+		: mode{other.mode}
+		, _this{std::move(other._this)}
+	{}
+
+	State::State(State const& other)
+		: mode{other.mode}
+		, _this{new Impl{*other._this}}
+	{}
+
+	State::~State()
+	{}
+
+	matrix_type const&
+	State::matrix(MatrixKind kind) const
+	{
+		switch (kind)
 		{
-			if (this->mode != other.mode)
-				throw Exception{"Trying to move state with a different mode"};
-			std::copy(other._matrices,
-			          other._matrices + (size_t)MatrixKind::_max_value,
-			          _matrices);
+		case MatrixKind::model:
+			return _this->model;
+		case MatrixKind::view:
+			return _this->view;
+		case MatrixKind::projection:
+			return _this->projection;
+		case MatrixKind::mvp:
+			return this->mvp();
+		default:
+			throw Exception{"Invalid matrix kind"};
 		}
-		return *this;
 	}
 
 	void
 	State::matrix(MatrixKind kind, matrix_type const& other)
 	{
 		ETC_TRACE.debug("Set matrix", kind, "to", other);
-		if (kind == MatrixKind::mvp)
+		switch (kind)
+		{
+		case MatrixKind::model:
+			_this->model = other;
+			break;
+		case MatrixKind::view:
+			_this->view = other;
+			break;
+		case MatrixKind::projection:
+			_this->projection = other;
+			break;
+		case MatrixKind::mvp:
 			throw Exception{"mvp Matrix is read only"};
-		_matrices[(size_t) kind] = other;
-		_mvp_dirty = true;
+		default:
+			throw Exception{"Invalid matrix kind"};
+		}
+		_this->mvp_dirty = true;
 	}
+
+	matrix_type const& State::model() const noexcept
+	{ return _this->model; }
+
+	matrix_type const& State::view() const noexcept
+	{ return _this->view; }
+
+	matrix_type const& State::projection() const noexcept
+	{ return _this->projection; }
+
+	matrix_type const& State::mvp() const noexcept
+	{
+		if (_this->mvp_dirty)
+		{
+			_this->mvp_dirty = false;
+			_this->mvp = _this->projection * _this->view * _this->model;
+		}
+		return _this->mvp;
+	}
+
+	State& State::model(matrix_type const& other) noexcept
+	{ _this->model = other; _this->mvp_dirty = true; return *this; }
+
+	State& State::view(matrix_type const& other) noexcept
+	{ _this->view = other; _this->mvp_dirty = true; return *this; }
+
+	State& State::projection(matrix_type const& other) noexcept
+	{ _this->projection = other; _this->mvp_dirty = true; return *this; }
 
 	State& State::scale(component_type const x,
 	                    component_type const y,
 	                    component_type const z) noexcept
-	{
-		_matrices[(size_t) MatrixKind::model] =
-			matrix::scale(this->model(), x, y, z);
-		_mvp_dirty = true;
-		return *this;
-	}
+	{ return this->model(matrix::scale(this->model(), x, y, z)); }
 
 	State& State::translate(component_type const x,
 	                        component_type const y,
 	                        component_type const z) noexcept
-	{
-		_matrices[(size_t) MatrixKind::model] =
-			matrix::translate(this->model(), x, y, z);
-		_mvp_dirty = true;
-		return *this;
-	}
+	{ return this->model(matrix::translate(this->model(), x, y, z)); }
 
 	State& State::rotate(units::Angle const angle,
 	                     vector_type const& axis) noexcept
-	{
-		_matrices[(size_t) MatrixKind::model] =
-			matrix::rotate(this->model(), angle, axis);
-		_mvp_dirty = true;
-		return *this;
-	}
+	{ return this->model(matrix::rotate(this->model(), angle, axis)); }
 
 	State& State::look_at(vector_type const& eye,
 	                      vector_type const& center,
 	                      vector_type const& up) noexcept
-	{
-		_matrices[(size_t) MatrixKind::view] = matrix::look_at(eye, center, up);
-		_mvp_dirty = true;
-		return *this;
-	}
+	{ return this->view(matrix::look_at(eye, center, up)); }
 
 	State& State::perspective(component_type const fov,
 	                          component_type const aspect,
 	                          component_type const near,
 	                          component_type const far) noexcept
-	{
-		_matrices[(size_t) MatrixKind::projection] =
-			matrix::perspective(fov, aspect, near, far);
-		_mvp_dirty = true;
-		return *this;
-	}
+	{ return this->projection(matrix::perspective(fov, aspect, near, far)); }
 
 	State& State::ortho(component_type const x,
 	                    component_type const y,
 	                    component_type const w,
 	                    component_type const h) noexcept
-	{
-		_matrices[(size_t) MatrixKind::projection] = matrix::ortho(x, y, w, h);
-		_mvp_dirty = true;
-		return *this;
-	}
+	{ return this->projection(matrix::ortho(x, y, w, h)); }
 
 }}} // !cube::gl::renderer
--- src/cube/gl/renderer/State.hpp
+++ src/cube/gl/renderer/State.hpp
@@ -19,52 +19,27 @@ namespace cube { namespace gl { namespace renderer {
 		typedef vector::Vector3<component_type> vector_type;
 	public:
 		Mode const mode;
-	protected:
-		matrix_type mutable _matrices[(size_t)MatrixKind::_max_value];
-		bool mutable        _mvp_dirty;
+	private:
+		struct Impl;
+		std::unique_ptr<Impl> _this;
 
 	public:
-		State(Mode const mode)
-			: mode(mode)
-			, _matrices{{}, {}, {}, {}}
-			, _mvp_dirty{false}
-		{}
-
-		State(State&& other)
-			: mode{other.mode}
-			, _mvp_dirty{other._matrices}
-		{
-			std::copy(other._matrices,
-			          other._matrices + (size_t)MatrixKind::_max_value,
-			          _matrices);
-		}
-
-		State(State const&) = default;
-		State& operator =(State&&) = delete;
-		State& operator =(State const&);
-
 		/**
-		 * Getter/setter for matrices
+		 * @brief State are constructible with a rendering mode and are fully
+		 *        move and copy constructible.
 		 */
-	public:
+		explicit
+		State(Mode const mode);
+		State(State&& other)
+			noexcept(std::is_nothrow_move_constructible<Impl>());
+		State(State const& other);
+		~State();
 
+	public:
 		/**
 		 * @brief Returns the matrix of kind @a kind
 		 */
-		inline
-		matrix_type const& matrix(MatrixKind kind) const
-		{
-			if (kind == MatrixKind::mvp && _mvp_dirty)
-			{
-				_mvp_dirty = false;
-				_matrices[(size_t) MatrixKind::mvp] =
-					_matrices[(size_t) MatrixKind::projection] *
-					_matrices[(size_t) MatrixKind::view] *
-					_matrices[(size_t) MatrixKind::model]
-				;
-			}
-			return _matrices[(size_t) kind];
-		}
+		matrix_type const& matrix(MatrixKind kind) const;
 
 		/**
 		 * @brief Set the matrix of kind @a kind
@@ -73,24 +48,20 @@ namespace cube { namespace gl { namespace renderer {
 		 */
 		void matrix(MatrixKind kind, matrix_type const& other);
 
+		/**
+		 * @brief Get a matrix.
+		 */
+		matrix_type const& model() const noexcept;
+		matrix_type const& view() const noexcept;
+		matrix_type const& projection() const noexcept;
+		matrix_type const& mvp() const noexcept;
 
-		inline
-		matrix_type const& mvp() const
-		{ return this->matrix(MatrixKind::mvp); }
-
-# define _CUBE_GL_RENDERER_RENDERER_STATE_MAT(name)                           \
-		inline                                                                \
-		matrix_type const& name() const                                       \
-		{ return _matrices[(size_t) MatrixKind::name]; }                      \
-                                                                              \
-		inline                                                                \
-		void name(matrix_type const& other)                                   \
-		{ _matrices[(size_t) MatrixKind::name] = other; _mvp_dirty = true; }  \
-	/**/
-		_CUBE_GL_RENDERER_RENDERER_STATE_MAT(model);
-		_CUBE_GL_RENDERER_RENDERER_STATE_MAT(view);
-		_CUBE_GL_RENDERER_RENDERER_STATE_MAT(projection);
-# undef _CUBE_GL_RENDERER_RENDERER_STATE_MAT
+		/**
+		 * @brief Set a matrix.
+		 */
+		State& model(matrix_type const& other) noexcept;
+		State& view(matrix_type const& other) noexcept;
+		State& projection(matrix_type const& other) noexcept;
 
 	public:
 		/**
