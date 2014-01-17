d1fbc7f035adbf412311355760a216fede8907cd
Raphaël Londeix <raphael.londeix@infinit.io>
2014-01-17 17:03:59 +0100

    cube.gl.text: Add simple text test.

---
+++ src/cube/gl/text_test.py
@@ -0,0 +1,26 @@
+import os
+
+from cube.test import Case
+from cube.gl.renderer.Painter_test import PainterSetup, painter_test
+from cube import gl
+
+class _(PainterSetup, Case):
+
+    font_path = os.path.join(os.path.dirname(__file__), 'font_test.ttf')
+    takao_font_path = os.path.join(os.path.dirname(__file__), 'font_test_takao.ttf')
+
+    def setUp(self):
+        super().setUp()
+        info = gl.font.get_infos(self.font_path)[0]
+        self.font = gl.Font(self.renderer, info, 48)
+
+    def tearDown(self):
+        self.font = None
+        super().tearDown()
+
+    @painter_test(gl.mode_2d)
+    def test_simple(self, painter):
+        text = gl.Text(self.font, "lol2")
+        text.color = gl.Color3f("yellow")
+        text.draw(painter)
+
