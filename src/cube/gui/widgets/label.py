7fe4cd136dcf75c412ba5e63cc3bb74ea615657d
Raphaël Londeix <raphael.londeix@infinit.io>
2014-01-17 17:07:39 +0100

    cube.gui.widgets.label: Make it work again.

--- src/cube/gui/widgets/label.py
+++ src/cube/gui/widgets/label.py
@@ -12,26 +12,6 @@ class Label(view.View):
         self.__text = None
 
     def _prepare(self, renderer):
-        self.__sp  = renderer.new_shader_program([
-            renderer.new_fragment_shader([
-                """
-            uniform sampler2D sampler0;
-            void main(void) {
-                float c = texture2D(sampler0, vec2(gl_TexCoord[0])).r;
-                if (c == 0.0) discard;
-                gl_FragColor = vec4(vec3(.1,.1,.1) , c);
-            }
-        """]),
-        renderer.new_vertex_shader(["""
-            uniform mat4 cube_ModelViewProjectionMatrix;
-            void main(void)
-            {
-                //gl_FrontColor = gl_Color;
-                gl_TexCoord[0] = gl_MultiTexCoord0;
-                gl_Position = cube_ModelViewProjectionMatrix * gl_Vertex;
-            }
-        """]),
-        ])
         self.__text = gl.Text(self.font, self.__str);
 
     @property
@@ -44,7 +24,5 @@ class Label(view.View):
         self.__text = gl.Text(self.font, self.__str);
 
     def render(self, painter):
-        self.__sp['cube_ModelViewProjectionMatrix'] = painter.state.mvp
-        with painter.bind([self.__sp]):
-            self.__text.draw(painter, self.__sp["sampler0"])
+        self.__text.draw(painter)
 
