9cb6d93daf057247f877372aa4e410fea318fdb8
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-04 18:25:28 +0100

    etc.sys.environ: Add lexical casts for environment variable.

--- src/etc/sys/environ.cpp
+++ src/etc/sys/environ.cpp
@@ -1,12 +1,22 @@
 #include "environ.hpp"
 
+#include <boost/algorithm/string.hpp>
+
 #include <cstdlib>
 
 namespace etc { namespace sys { namespace environ {
 
+	using exception::Exception;
+	using exception::KeyError;
+	using exception::ValueError;
+
+	static
+	char const* raw_get(std::string const& key)
+	{ return ::getenv(key.c_str()); }
+
 	std::string get(std::string const& key)
 	{
-		char const* val = ::getenv(key.c_str());
+		char const* val = raw_get(key);
 		if (val == nullptr)
 			throw KeyError(key);
 		return std::string{val};
@@ -15,10 +25,27 @@ namespace etc { namespace sys { namespace environ {
 	std::string get(std::string const& key,
 	                std::string const& default_value)
 	{
-		char const* val = ::getenv(key.c_str());
+		char const* val = raw_get(key);
 		if (val == nullptr)
 			return default_value;
 		return std::string{val};
+	}
+
+	template<>
+	ETC_API bool as<bool>(std::string const& key)
+	{
+		char const* val = raw_get(key);
+		if (val == nullptr)
+			return false;
+		std::string value{val};
+		boost::to_lower(value);
+		static std::string true_values[] = {"1", "yes", "ok", "on", "true"};
+		static std::string false_values[] = {"0", "no", "ko", "off", "false"};
+		for (auto const& s: true_values)
+			if (s == value) return true;
+		for (auto const& s: false_values)
+			if (s == value) return false;
+		throw ValueError{"Cannot cast '" + value + "' to bool"};
 	}
 
 	bool contains(std::string const& key) ETC_NOEXCEPT
@@ -33,7 +60,7 @@ namespace etc { namespace sys { namespace environ {
 #else
 		if (::setenv(key.c_str(), value.c_str(), 1) < 0)
 #endif
-			throw std::runtime_error("Cannot set env var " + key);
+			throw Exception("Cannot set env var " + key);
 		return old;
 	}
 
@@ -52,7 +79,7 @@ namespace etc { namespace sys { namespace environ {
 #else
 			if (::setenv(key.c_str(), value.c_str(), 1) < 0)
 #endif
-				throw std::runtime_error("Cannot set env var " + key);
+				throw Exception("Cannot set env var " + key);
 			return value;
 		}
 		return std::string{old_value};
--- src/etc/sys/environ.hpp
+++ src/etc/sys/environ.hpp
@@ -25,6 +25,30 @@ namespace etc { namespace sys { namespace environ {
 	                        std::string const& value);
 
 	/**
+	 * @brief Cast an environment variable to a type T.
+	 */
+	template<typename T>
+	ETC_API T as(std::string const& key);
+
+	template<typename T>
+	ETC_API T try_as(std::string const& key)
+	{
+		try { return as<T>(key); }
+		catch (exception::ValueError const&) { return T{}; }
+	}
+
+	/**
+	 * @brief Cast an environnement variable to a boolean.
+	 *
+	 * Returns true if an environment variable lower cased value is either "1",
+	 * "yes", "ok", "on" or "true". It returns false if the variable is not
+	 * defined, or when the value is either "0", "no", "ko", "off" or "false".
+	 * In other cases, it throws a ValueError exception.
+	 */
+	template<>
+	ETC_API bool as<bool>(std::string const& key);
+
+	/**
 	 * @brief Set a value into the environment and returns the old value or an
 	 * empty string.
 	 */
@@ -42,17 +66,6 @@ namespace etc { namespace sys { namespace environ {
 	 * @brief Returns whether or not a variable is in the environment.
 	 */
 	ETC_API bool contains(std::string const& key) ETC_NOEXCEPT;
-
-	/**
-	 * Exception thrown when the key is not found.
-	 */
-	struct ETC_API_EXCEPTION KeyError
-		: public etc::exception::Exception
-	{
-		KeyError(std::string const& msg)
-			: etc::exception::Exception(msg)
-		{}
-	};
 
 }}}
 
