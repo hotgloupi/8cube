0e4953971eca938b7f20f3f392fe66def500520b
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-09 19:53:29 +0100

    etc: Make cast overloads aware of pointer types.

--- src/etc/cast.hpp
+++ src/etc/cast.hpp
@@ -7,21 +7,53 @@
 
 namespace etc {
 
+	namespace detail {
+
+		template<typename T, typename Ret = void>
+		struct enable_polymorphic
+			: std::enable_if<
+				std::has_virtual_destructor<
+					typename std::remove_pointer<
+						typename std::remove_reference<T>::type
+					>::type
+				>::value
+				, Ret
+			>
+		{};
+
+		template<typename T, typename Ret = void>
+		struct disable_polymorphic
+			: std::enable_if<
+				!std::has_virtual_destructor<
+					typename std::remove_pointer<
+						typename std::remove_reference<T>::type
+					>::type
+				>::value
+				, Ret
+			>
+		{};
+
+	}
+
+	// Non-polymorphic cast.
 	template<typename To, typename From>
-	To cast(From&& from)
+	auto cast(From&& from)
+		-> typename detail::disable_polymorphic<From, To>::type
 	{ return static_cast<To>(std::forward<From>(from)); }
 
+	// Polymorphic pointer cast.
 	template<typename To, typename From>
-	typename std::enable_if<std::has_virtual_destructor<From>::value, To>::type
-	cast(From* from)
+	auto cast(From* from)
+		-> typename detail::enable_polymorphic<From, To>::type
 	{
 		ETC_ASSERT_EQ(dynamic_cast<To>(from), static_cast<To>(from));
 		return static_cast<To>(from);
 	}
 
+	// Polymorphic reference cast.
 	template<typename To, typename From>
-	typename std::enable_if<std::has_virtual_destructor<From>::value, To>::type
-	cast(From& from)
+	auto cast(From& from)
+		-> typename detail::enable_polymorphic<From, To>::type
 	{
 		typedef typename std::remove_reference<To>::type* to_ptr_type;
 		ETC_ASSERT_EQ(
@@ -31,6 +63,7 @@ namespace etc {
 		return static_cast<To>(from);
     }
 
+	// Unique pointer cast.
 	template<typename To, typename From>
 	std::unique_ptr<To> cast(std::unique_ptr<From> from)
 	{
@@ -39,6 +72,7 @@ namespace etc {
 		return std::move(res);
 	}
 
+	// Shared pointer cast.
 	template<typename To, typename From>
 	std::shared_ptr<To> cast(std::shared_ptr<From> const& from)
 	{
