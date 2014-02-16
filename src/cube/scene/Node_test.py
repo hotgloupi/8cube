0d761b84caf9cd42edfe4996138deb7502df925a
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-17 00:25:32 +0100

    cube.scene: Add Node inheritance and ref count tests.

--- src/cube/scene/Node_test.py
+++ src/cube/scene/Node_test.py
@@ -1,5 +1,6 @@
 from cube.test import Case
 from . import Graph, Node
+from . import visit
 
 class _(Case):
 
@@ -37,4 +38,68 @@ class _(Case):
         child = g.root.emplace_child(Node, "lol")
         self.assertEqual(child.name, "lol")
         self.assertEqual(g.size, 2)
+
+    def test_children(self):
+        g = Graph()
+        g.root.emplace_child(Node, "lol")
+        self.assertEqual(g.root.children[0].name, "lol")
+
+class RefCount(Case):
+
+    def test_custom_node(self):
+        class N(Node):
+            pass
+        n = N("lol")
+        import gc
+        self.assertEqual(len(gc.get_referrers(n)), 1)
+
+class Inheritance(Case):
+
+    def setUp(self):
+        self.graph = Graph()
+
+    def tearDown(self):
+        self.graph = None
+        import gc
+        gc.collect()
+
+    class MyNode(Node):
+        def __init__(self):
+            super().__init__("my node")
+            self.lol = 42
+
+
+    def test_emplace(self):
+        my_node = self.graph.root.emplace_child(self.MyNode)
+        self.assertEqual(my_node.name, "my node")
+        self.assertEqual(my_node.lol, 42)
+
+        my_node = self.graph.root.children[0]
+        self.assertEqual(my_node.name, "my node")
+        self.assertEqual(my_node.lol, 42)
+
+    def test_visit(self):
+        my_node = self.graph.root.emplace_child(self.MyNode)
+        visited = []
+        my_node.visit(lambda n: visited.append(n))
+        self.assertEqual(len(visited), 1)
+        self.assertEqual(visited[0].name, 'my node')
+        self.assertEqual(visited[0].lol, 42)
+
+   # def test_traverse(self):
+   #     self.graph.root.emplace_child(self.MyNode)
+   #     visited = []
+   #     self.graph.traverse(lambda n: visited.append(n))
+
+    def test_dfs_bfs(self):
+        self.graph.root.emplace_child(self.MyNode)
+        visited = []
+        visit.bfs(self.graph, lambda n: visited.append(n))
+        self.assertEqual(visited[1].name, "my node")
+        self.assertEqual(visited[1].lol, 42)
+
+    def test_children(self):
+        self.graph.root.emplace_child(self.MyNode)
+        self.assertEqual(self.graph.root.children[0].name, "my node")
+        self.assertEqual(self.graph.root.children[0].lol, 42)
 
