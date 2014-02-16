b146eab1ba82e729d6cd9d3e234b000137e55184
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-17 00:35:48 +0100

    cube.scene: Add Graph new constructors implem.

--- src/cube/scene/Graph.cpp
+++ src/cube/scene/Graph.cpp
@@ -28,8 +28,26 @@ namespace cube { namespace scene {
 		_this->root = &this->emplace<Node>("root");
 	}
 
+	Graph::Graph(Graph&& other)
+		: _this{std::move(other._this)}
+	{}
+
+	Graph::Graph(std::unique_ptr<Node> node)
+		: _this{new Impl}
+	{
+		ETC_TRACE_CTOR();
+		_this->root = &this->insert(std::move(node));
+	}
+
+	Graph::Graph(Node& node, node_deleter_type deleter)
+		: _this{new Impl}
+	{
+		ETC_TRACE_CTOR();
+		_this->root = &this->insert(node, std::move(deleter));
+	}
+
 	Graph::~Graph()
-	{ ETC_TRACE_DTOR(); }
+	{ ETC_TRACE_DTOR(); _this.reset(); }
 
 	Node& Graph::root() ETC_NOEXCEPT
 	{ return *_this->root; }
@@ -81,7 +99,7 @@ namespace cube { namespace scene {
 		return node;
 	}
 
-	std::unique_ptr<Node> Graph::remove(Node& node)
+	std::shared_ptr<Node> Graph::remove(Node& node)
 	{
 		ETC_LOG.debug("Remove", node, "from index");
 
@@ -92,10 +110,10 @@ namespace cube { namespace scene {
 		auto id = node.id();
 		boost::clear_vertex(id, _this->graph);
 		boost::remove_vertex(id, _this->graph);
-		ETC_ASSERT_EQ(ptr.unique(), 2);
+		ETC_ASSERT_EQ(ptr.unique(), true);
 
 		ptr->detach(*this);
-		return std::unique_ptr<Node>{ptr.get()};
+		return ptr;
 	}
 
 	void Graph::connect(Node& from, Node& to)
@@ -103,6 +121,18 @@ namespace cube { namespace scene {
 		ETC_ASSERT_EQ(_this->graph[from.id()].get(), &from);
 		ETC_ASSERT_EQ(_this->graph[to.id()].get(), &to);
 		boost::add_edge(from.id(), to.id(), _this->graph);
+	}
+
+
+	std::vector<Node*> Graph::children(Node& node)
+	{
+		std::vector<Node*> res;
+		Impl::out_edge_iterator it, end;
+		for (boost::tie(it, end) = boost::out_edges(node.id(), _this->graph);
+		     it != end;
+		     ++it)
+			res.push_back(_this->graph[boost::target(*it, _this->graph)].get());
+		return res;
 	}
 
 	void Graph::traverse(NodeVisitor<Node>& visitor)
