6b13acc6b086e0e7fc8bcacca0170c6ef49e7e2a
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-17 00:32:32 +0100

    cube.scene: Test graph constructor with root node.

--- src/cube/scene/Graph_test.py
+++ src/cube/scene/Graph_test.py
@@ -10,3 +10,18 @@ class _(Case):
         self.assertIsInstance(g.root, Node)
         self.assertEqual(g.size, 1)
 
+    def test_node_ctor(self):
+        n = Node("LOL!")
+        g = Graph(n)
+        self.assertEqual(g.root.name, "LOL!")
+
+    def test_custom_node_ctor(self):
+        class C(Node):
+            def __init__(self):
+                super().__init__("Megalol")
+                self.lol = 42
+        c = C()
+        g = Graph(c)
+        self.assertEqual(g.root.name, "Megalol")
+        self.assertEqual(g.root.lol, 42)
+
