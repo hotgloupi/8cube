5a6dc254cbef04709974f1bed21c8379f7d381de
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-23 22:49:36 +0100

    cube.resource.Resource: Add shared_from_this() weak variants.

--- src/cube/resource/Resource.hpp
+++ src/cube/resource/Resource.hpp
@@ -4,6 +4,7 @@
 # include "fwd.hpp"
 
 # include <cube/api.hpp>
+# include <cube/exception.hpp>
 
 # include <etc/compiler.hpp>
 
@@ -18,10 +19,12 @@ namespace cube { namespace resource {
 		: public std::enable_shared_from_this<Resource>
 	{
 	private:
+		// _id and _manager are set whenever a resource is managed.
 		id_type     _id;
 		Manager*    _manager;
 
 	public:
+		/// Non managed resource.
 		inline
 		Resource() ETC_NOEXCEPT
 			: _id{0}
@@ -31,13 +34,47 @@ namespace cube { namespace resource {
 		virtual
 		~Resource();
 
-		inline
-		id_type id() const ETC_NOEXCEPT
+		/// id of a managed resource or 0.
+		inline id_type id() const ETC_NOEXCEPT
 		{ return _id; }
 
-		inline
-		bool managed() const ETC_NOEXCEPT
+		/// True when managed.
+		inline bool managed() const ETC_NOEXCEPT
 		{ return _id != 0; }
+
+	// Pretty ways to get the underlying pointer.
+	private:
+		enum PtrKind { shared_ptr_only, some_ptr };
+		template<PtrKind kind>
+		struct cast_to
+		{
+			Resource* _resource;
+			cast_to(Resource* resource) : _resource{resource} {}
+			template<typename T>
+			operator std::shared_ptr<T>() const
+			{
+				try
+				{ return std::dynamic_pointer_cast<T>(_resource->shared_from_this()); }
+				catch (std::bad_weak_ptr const&) {}
+				if (kind == shared_ptr_only)
+					throw exception::Exception{"Not shared"};
+				return std::shared_ptr<T>{
+					static_cast<T*>(_resource),
+					[] (T*) {}
+				};
+			}
+		};
+	public:
+		/// Build a shared_ptr or throw if the resource is not associated to
+		/// a shared pointer.
+		cast_to<shared_ptr_only> shared_ptr()
+		{ return cast_to<shared_ptr_only>(this); }
+
+		/// Build a shared_ptr whether or not the resource was associated to
+		/// a shared pointer. In the latter case, you are responsible to keep
+		/// this instance alive while any ptr instance is in use.
+		cast_to<some_ptr> ptr()
+		{ return cast_to<some_ptr>(this); }
 
 	private:
 		id_type manage(Manager& manager,
