5328cdbaaa97808b0ef6eb50c8c666599dea7f9e
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-23 23:09:27 +0100

    cube.gui.application: Default window size is 16:9 compliant.

--- src/cube/gui/application.py
+++ src/cube/gui/application.py
@@ -4,7 +4,7 @@ import time
 import cube
 
 class Application(cube.Application):
-    def __init__(self, name="Cube app", width=640, height=480):
+    def __init__(self, name="Cube app", width=800, height=480):
         events = [
             'idle', 'quit',
         ]
