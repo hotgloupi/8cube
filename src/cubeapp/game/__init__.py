0fffaacb7d9c5d7e44b4dca5c7a33fd35ffbbd2e
Raphaël Londeix <raphael.londeix@infinit.io>
2014-02-23 22:52:11 +0100

    cubeapp.game: Add doc and expose Controller class.

--- src/cubeapp/game/__init__.py
+++ src/cubeapp/game/__init__.py
@@ -1,12 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 from . import event
-from .event.manager import Manager as EventManager
 from . import input
+from .controller import Controller
 
 from cube import gl
 
-def load(games_path, name, window, client):
+def load(games_path, name, window):
+    """Create a Game instance by loading a game called `name` in `games_path`.
+    `window` instance is forwarded to the constructor of the Game class found
+    in "${games_path}/${name}/game.py".
+    """
     import sys
     old_python_path = sys.path
     sys.path = [games_path] + sys.path
@@ -14,24 +18,45 @@ def load(games_path, name, window, client):
         game_module = __import__(name + '.game', fromlist=['game'])
     finally:
         sys.path = old_python_path
-    return game_module.Game(window, client)
+    return game_module.Game(window)
 
 class Game():
-    """Base class for every games
+    """Base class for every games.
+
+    A game instance exposes the following attributes:
+        * window: The operating system window instance.
+        * renderer: The renderer.
+        * world: An optional World instance.
+        * view: An optional cube.gui.View object.
+        * event_manager: The event manager (game related).
+        * input_translator: Exposes bindings as event channels.
     """
 
-    def __init__(self, window, client, bindings = {}, world = None, view = None):
+    def __init__(self, window, bindings = {}, world = None, view = None):
+        """Construct a game instance.
+
+        # Positional parameters
+            * window: An instance of cube.gui.Window.
+
+        # Optional parameters
+            * bindings: Bindings map (see cubeapp.game.input.Translator).
+            * world: An instance of cubeapp.world.World.
+            * view: An instance of cube.gui.View.
+
+        # Bindings description
+            Bindings constist of a dictionary where keys are simple string, and
+            values are of the following form:
+        """
         self.window = window
         self.renderer = window.renderer
-        self.client = client
         self.world = world
-        self.event_manager = EventManager()
+        self.view = view
+        self.event_manager = event.Manager()
         self.input_translator = input.Translator(window, bindings)
         self.projection_matrix = gl.matrix.perspective(
             45, 1, 0.005, 300.0
         )
         self.window.inputs.on_quit.connect(self._on_quit)
-        self.view = view
 
     @property
     def player(self):
