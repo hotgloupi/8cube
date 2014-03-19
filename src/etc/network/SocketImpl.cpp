4b9f42992cc7802be61fa5738f2d76b6ac5bd9ca
Raphaël Londeix <raphael.londeix@infinit.io>
2014-03-19 14:19:28 +0100

    etc.network: Add configuration and connect methods to Socket impl.

--- src/etc/network/SocketImpl.cpp
+++ src/etc/network/SocketImpl.cpp
@@ -1,22 +1,27 @@
 #include "SocketImpl.hpp"
-
-#include <etc/exception.hpp>
+#include "Exception.hpp"
 
 namespace etc { namespace network {
 
+	Socket::Impl::~Impl()
+	{}
+
 	Socket::buffer_type Socket::Impl::read(etc::size_type s)
-	{ throw exception::Exception{"Invalid operation"}; }
+	{ throw InvalidOperation{}; }
 
 	size_t Socket::Impl::write(Socket::buffer_type const& data)
-	{ throw exception::Exception{"Invalid operation"}; }
+	{ throw InvalidOperation{}; }
 
-	void Socket::Impl::bind(std::string address, uint16_t const port)
-	{ throw exception::Exception{"Invalid operation"}; }
+	void Socket::Impl::bind(std::string const& address, uint16_t const port)
+	{ throw InvalidOperation{}; }
 
 	void Socket::Impl::listen()
-	{ throw exception::Exception{"Invalid operation"}; }
+	{ throw InvalidOperation{}; }
 
 	Socket Socket::Impl::accept()
-	{ throw exception::Exception{"Invalid operation"}; }
+	{ throw InvalidOperation{}; }
+
+	void Socket::Impl::connect(std::string const& address, uint16_t port)
+	{ throw InvalidOperation{}; }
 
 }}
--- src/etc/network/SocketImpl.hpp
+++ src/etc/network/SocketImpl.hpp
@@ -9,11 +9,13 @@ namespace etc { namespace network {
 
 	struct Socket::Impl
 	{
+		virtual int configuration() const = 0;
 		virtual Socket::buffer_type read(etc::size_type s);
 		virtual size_t write(Socket::buffer_type const& data);
-		virtual void bind(std::string address, uint16_t const port);
+		virtual void bind(std::string const& address, uint16_t const port);
 		virtual void listen();
 		virtual Socket accept();
+		virtual void connect(std::string const& address, uint16_t port);
 		virtual ~Impl();
 	};
 
