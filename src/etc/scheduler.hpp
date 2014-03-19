4a3f7ba1e20b3413c6222c44f43dc881cba42c37
Raphaël Londeix <raphael.londeix@infinit.io>
2014-03-19 14:15:56 +0100

    etc.scheduler: Add spawn shortcut function.

--- src/etc/scheduler.hpp
+++ src/etc/scheduler.hpp
@@ -7,6 +7,16 @@ namespace etc { namespace scheduler {
 
 	Scheduler& current();
 
+	template<typename Callable>
+	auto spawn(std::string name, Callable&& cb)
+		-> decltype(std::forward<Callable>(cb)())
+	{ current().spawn(std::move(name), [&] (Context&) { std::forward<Callable>(cb)(); }); }
+
+	template<typename Callable>
+	auto spawn(std::string name, Callable&& cb)
+		-> decltype(std::forward<Callable>(cb)(std::declval<Context&>()))
+	{ current().spawn(std::move(name), cb); }
+
 }}
 
 #endif
