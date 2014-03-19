fe137f019fe13f7db34570e5ebcce74a3d0ceba1
Raphaël Londeix <raphael.londeix@infinit.io>
2014-03-19 20:52:53 +0100

    etc.network: More readable socket impl text representation.

--- src/etc/network/SocketImpl.cpp
+++ src/etc/network/SocketImpl.cpp
@@ -1,6 +1,8 @@
 #include "SocketImpl.hpp"
 #include "Exception.hpp"
 
+#include <etc/str.hpp>
+
 namespace etc { namespace network {
 
 	Socket::Impl::~Impl()
@@ -23,5 +25,25 @@ namespace etc { namespace network {
 
 	void Socket::Impl::connect(std::string const& address, uint16_t port)
 	{ throw InvalidOperation{}; }
+
+	void Socket::Impl::print(std::ostream& out) const ETC_NOEXCEPT
+	{
+		out << '<'
+			<< str(ETC_TYPE_STRING(*this))
+			.replace("Impl", "")
+			.replace("etc::network::", "")
+			.replace("(anonymous namespace)::", "")
+			.replace("boost::asio::", "")
+			.replace("basic_", "")
+			.replace("socket_", "")
+			.replace(", acceptor_service<ip::tcp> ", "")
+			.replace(", stream_service<ip::tcp> ", "")
+			.replace("> >", ">>")
+			.replace("<", "(")
+			.replace(">", ")")
+			<< " at " << this << '>';
+		;
+
+	}
 
 }}
--- src/etc/network/SocketImpl.hpp
+++ src/etc/network/SocketImpl.hpp
@@ -4,10 +4,12 @@
 # include "Socket.hpp"
 
 # include <etc/types.hpp>
+# include <etc/printable.hpp>
 
 namespace etc { namespace network {
 
 	struct Socket::Impl
+		: public Printable
 	{
 		virtual int configuration() const = 0;
 		virtual Socket::buffer_type read(etc::size_type s);
@@ -16,6 +18,7 @@ namespace etc { namespace network {
 		virtual void listen();
 		virtual Socket accept();
 		virtual void connect(std::string const& address, uint16_t port);
+		void print(std::ostream& out) const ETC_NOEXCEPT override;
 		virtual ~Impl();
 	};
 
