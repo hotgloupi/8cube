2b62ac81da0a2ba7679569000d8bf555f51c4763
Raphaël Londeix <raphael.londeix@infinit.io>
2014-04-06 00:24:43 +0200

    Disable SDL audio and move cURL into configure.py

--- .config/project.py
+++ .config/project.py
@@ -81,70 +81,6 @@ class Assimp(CMakeDependency):
                 )
             )
 
-class cURL(CMakeDependency):
-
-    def __init__(self,
-                 build,
-                 compiler,
-                 source_directory,
-                 zlib = None,
-                 openssl = None,
-                 with_cookies = True,
-                 with_crypto_auth = True,
-                 with_dict = True,
-                 with_file = True,
-                 with_ftp = True,
-                 with_http = True,
-                 with_ldap = True,
-                 with_ldaps = True,
-                 with_telnet = True,
-                 with_tftp = True,
-                 use_ares = False,
-                 hidden_visibility = True,
-                 shared = True):
-        if not with_ldap or not openssl:
-            with_ldaps = False
-        if not openssl:
-            with_crypto_auth = False
-        configure_variables = [
-            ('CURL_ZLIB', bool(zlib)),
-            ('CMAKE_USE_OPENSSL', bool(openssl)),
-            ('CURL_DISABLE_COOKIES', not with_cookies),
-            ('CURL_DISABLE_CRYPTO_AUTH', not with_crypto_auth),
-            ('CURL_DISABLE_DICT', not with_dict),
-            ('CURL_DISABLE_FILE', not with_file),
-            ('CURL_DISABLE_FTP', not with_ftp),
-            ('CURL_DISABLE_HTTP', not with_http),
-            ('CURL_DISABLE_LDAP', not with_ldap),
-            ('CURL_DISABLE_LDAPS', not with_ldaps),
-            ('CURL_DISABLE_TELNET', not with_telnet),
-            ('CURL_DISABLE_TFTP', not with_tftp),
-            ('CURL_USE_ARES', use_ares),
-            ('HIDDEN_VISIBILITY', hidden_visibility),
-        ]
-        if not shared:
-            configure_variables.append(('CURL_STATICLIB', True))
-        super().__init__(
-            build,
-            "cURL",
-            compiler = compiler,
-            source_directory = source_directory,
-            libraries = [
-                {
-                    'prefix': 'lib',
-                    'name': 'curl',
-                    'shared': shared,
-                }
-            ],
-            configure_variables = configure_variables
-        )
-        if not shared and platform.IS_LINUX:
-            self.libraries.append(c.libraries.simple('idn', compiler, system = True))
-
-
-
-
-
 class GLM(Dependency):
     def __init__(self, build, compiler, source_directory):
         super().__init__(
@@ -245,7 +181,9 @@ def main(project, build):
     )
 
     curl = build.add_dependency(
-        cURL, c_compiler, "deps/curl-7.35.0",
+        c.libraries.CURLDependency,
+        c_compiler,
+        "deps/curl-7.35.0",
         shared = False, #platform.IS_WINDOWS,
         with_ldap = False,
         with_ldaps = False,
@@ -312,6 +250,7 @@ def main(project, build):
         c_compiler,
         'deps/SDL',
         shared = platform.IS_WINDOWS,
+        audio = False,
     )
     sdl_image = build.add_dependency(
         c.libraries.SDLImageDependency, c_compiler, 'deps/SDL_image',
@@ -367,15 +306,17 @@ def main(project, build):
             c.libraries.simple(name, compiler, system = True) for name in ['z', 'bz2',]
         )
         if platform.IS_LINUX:
-            base_libraries.extend(
-                c.libraries.simple(name, compiler, system = True) for name in ['audio',]
-            )
+            pass
+            # SDL audio disabled
+            #base_libraries.extend(
+            #    c.libraries.simple(name, compiler, system = True) for name in ['audio',]
+            #)
         elif platform.IS_MACOSX:
             base_libraries.extend(
                 c.libraries.simple(name, compiler, macosx_framework = True)
                 for name in [
-                    'ForceFeedback', 'IOKit', 'Cocoa', 'Carbon', 'AudioUnit', 'CoreAudio',
-                    'AudioToolbox',
+                    'ForceFeedback', 'IOKit', 'Cocoa', 'Carbon',
+                    # 'AudioUnit', 'CoreAudio', 'AudioToolbox', # SDL Audio disabled
                 ]
             )
 
