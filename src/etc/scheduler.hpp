ade9416d94e6b37a4cc6ab18eea428e119011645
Raphaël Londeix <raphael.londeix@infinit.io>
2014-04-08 00:29:12 +0200

    etc.scheduler: Fix MSVC compat.

--- src/etc/scheduler.hpp
+++ src/etc/scheduler.hpp
@@ -7,15 +7,32 @@ namespace etc { namespace scheduler {
 
 	Scheduler& current();
 
-	template<typename Callable>
-	auto spawn(std::string name, Callable&& cb)
-		-> decltype(std::forward<Callable>(cb)())
-	{ current().spawn(std::move(name), [&] (Context&) { std::forward<Callable>(cb)(); }); }
+
+	//template<typename Callable>
+	//auto spawn(std::string name, Callable cb);
+
+	// spawn implem
+	namespace details {
+		template<typename Callable>
+		auto spawn_imp(std::string name, Callable cb, int)
+			-> decltype(std::forward<Callable>(cb)())
+		{
+			return current().spawn(
+				std::move(name),
+				[&] (Context&) { std::forward<Callable>(cb)(); }
+			);
+		}
+
+		template<typename Callable>
+		auto spawn_imp(std::string name, Callable cb, long)
+			-> decltype(std::forward<Callable>(cb)(std::declval<Context&>()))
+		{ return current().spawn(std::move(name), cb); }
+	}
 
 	template<typename Callable>
-	auto spawn(std::string name, Callable&& cb)
-		-> decltype(std::forward<Callable>(cb)(std::declval<Context&>()))
-	{ current().spawn(std::move(name), cb); }
+	auto spawn(std::string name, Callable cb)
+		-> decltype(details::spawn_imp(std::move(name), std::move(cb), 0))
+	{ return details::spawn_imp(std::move(name), std::move(cb), 0); }
 
 }}
 
