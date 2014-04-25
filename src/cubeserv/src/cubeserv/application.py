a96fbdd4374810a7d75f4ad5f8c07b9dcc49d9ef
Raphaël Londeix <raphael.londeix@infinit.io>
2014-04-25 20:18:41 +0200

    cubeserv.application: Parse release file names.

--- src/cubeserv/src/cubeserv/application.py
+++ src/cubeserv/src/cubeserv/application.py
@@ -1,43 +1,89 @@
 
+import json
 import os
 import threading
 import time
+import zipfile
 
 from . import http
 
 class Application:
 
     def __init__(self, directory, address):
-        self.directory = os.path.abspath(directory)
-        self.address = self._split_address(address)
-        self.running = False
+        self.__directory = os.path.abspath(directory)
+        self.__address = self._split_address(address)
+        self.__running = False
 
     def run(self):
-        self.running = True
+        self.__running = True
         thread = threading.Thread(target = self.__update_list)
         try:
-            self.server = http.Server(self.address)
+            self.server = http.Server(self.__address)
             thread.start()
             self.server.run()
         finally:
-            self.running = False
+            self.__running = False
             thread.join()
 
+    def stop(self):
+        if self.__running:
+            self.__running = False
+            self.server.shutdown()
+
+    @property
+    def server_address(self):
+        if not self.__running:
+            raise Exception("Not running")
+        return self.server.server_address
+
+    @property
+    def directory(self):
+        return self.__directory
+
     def __update_list(self, period = 10):
-        while self.running:
+        while self.__running:
             self.server.frameworks = self.get_frameworks()
             slept = 0
             start = time.time()
-            while slept < period and self.running:
+            while slept < period and self.__running:
                 time.sleep(.01)
                 slept = time.time() - start
 
-    def get_frameworks(self):
-        files = os.listdir(self.directory)
-        for f in files:
-            yield {
-                'file': os.path.join(self.directory, f),
+
+    def __get_framework(self, file):
+            parts = file.split('-')
+            if len(parts) == 7:
+                os_, arch, type, compiler, version, build, sha1 = parts
+            elif len(parts) == 6:
+                os_, arch, type, compiler, version, sha1 = parts
+                build = 0
+            else:
+                raise Exception("Not a valid file name")
+            sha1 = sha1.split('.')[0] # remove .zip ext
+            with zipfile.ZipFile(os.path.join(self.directory, file)) as f:
+                with f.open(type + '-' + compiler + '/.manifest') as manifest_file:
+                    manifest = json.loads(manifest_file.read().decode('utf8'))
+            return {
+                'file': file,
+                'os': os_,
+                'arch': arch,
+                'type': type,
+                'compiler': compiler,
+                'version': version,
+                'build': int(build),
+                'sha1': sha1,
             }
+
+    def get_frameworks(self):
+        files = os.listdir(self.__directory)
+        for f in files:
+            if not f.endswith('.zip'):
+                continue
+            try:
+                yield self.__get_framework(f)
+            except Exception as e:
+                print("Ignore", f, ':', e)
+                pass
 
     @staticmethod
     def _split_address(addr):
