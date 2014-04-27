7a908dad329832114aefd20fde75ea1bd2007bb9
Raphaël Londeix <raphael.londeix@infinit.io>
2014-04-27 07:59:02 +0200

    cube.scene: Bind transform and content nodes.

--- src/cube/scene/__init__.py
+++ src/cube/scene/__init__.py
@@ -1,5 +1,7 @@
 from .Scene import Scene, from_string, from_file
 
 from .Node import Node
+from .Transform import Transform
+from .ContentNode import ContentNodeMesh, ContentNodeMaterial
 from .Graph import Graph
 from .visit import *
