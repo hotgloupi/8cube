7fcf13c1cf5b8bea3406ad0423ec7ce06f64681a
Raphaël Londeix <raphael.londeix@infinit.io>
2014-04-27 10:19:58 +0200

    cube.scene: export Drawable and Bindable content nodes.

--- src/cube/scene/__init__.py
+++ src/cube/scene/__init__.py
@@ -2,6 +2,9 @@ from .Scene import Scene, from_string, from_file
 
 from .Node import Node
 from .Transform import Transform
-from .ContentNode import ContentNodeMesh, ContentNodeMaterial
+from .ContentNode import ContentNodeBindable
+from .ContentNode import ContentNodeDrawable
+from .ContentNode import ContentNodeMaterial
+from .ContentNode import ContentNodeMesh
 from .Graph import Graph
 from .visit import *
