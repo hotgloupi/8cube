d559033d34d474a9556b9032a159f8ecb98bfd30
Raphaël Londeix <raphael.londeix@infinit.io>
2014-05-04 23:03:26 +0200

    cube.scene: Add content type factory.

--- src/cube/scene/__init__.py
+++ src/cube/scene/__init__.py
@@ -6,5 +6,25 @@ from .ContentNode import ContentNodeBindable
 from .ContentNode import ContentNodeDrawable
 from .ContentNode import ContentNodeMaterial
 from .ContentNode import ContentNodeMesh
+from .ContentNode import ContentNodeLight
 from .Graph import Graph
 from .visit import *
+
+from cube import gl
+
+def content_node_type(value):
+    node_type = {
+        gl.Light: ContentNodeLight,
+        gl.Material: ContentNodeMaterial,
+        gl.Mesh: ContentNodeMesh,
+    }.get(type(value))
+
+    if node_type is not None:
+        return node_type
+
+    if isinstance(value, gl.Bindable):
+        return ContentNodeBindable
+    elif isinstance(value, gl.Drawable):
+        return ContentNodeDrawable
+
+    raise Exception("Couldn't find any ContentNode type for '%s'" % value)
