5463a75e746cb316fecbf7fc0751d6c8752a6c0a
Raphaël Londeix <raphael.londeix@infinit.io>
2014-07-02 13:17:53 +0200

    Improve update script.

--- deps/update.sh
+++ deps/update.sh
@@ -7,6 +7,8 @@ SCRIPT=`basename "$0"`
 SCRIPT_DIR=`python -c "import os;print(os.path.abspath(os.path.dirname('$0')))"`
 cd "${SCRIPT_DIR}"
 
+[ -n "$VERBOSE" ] && set -x
+
 abspath()
 {
 	python -c 'import os, sys; print(os.path.abspath(os.path.join(*((len(sys.argv) > 1) and sys.argv[1:] or ["."]))))' "$@"
@@ -29,14 +31,19 @@ usage()
 	fatal "Usage: $SCRIPTCMD"
 }
 
+try()
+{
+	"$@" || fatal "Coudn't execute $*"
+}
+
 [ -z "`which hg 2> /dev/null`" ] && fatal "Mercurial not installed (hg command not found)"
 [ -z "`which svn 2> /dev/null`" ] && fatal "Subversion not installed (svn command not found)"
 if [ ! -z "`which wget 2> /dev/null`" ]
 then
 	fetch()
 	{
-			debug "wget $1 -> $2"
-		wget "$1" -O "$2"
+		debug "wget $1 -> $2"
+		wget --no-check-certificate "$1" -O "$2"
 	}
 else
 	if [ ! -z "`which curl 2> /dev/null`" ]
@@ -57,9 +64,9 @@ PYTHON_DIR=Python-${PYTHON_VERSION}
 debug "Updating ${PYTHON_DIR}"
 if [ ! -d ${PYTHON_DIR} ]
 then
-	hg clone http://hg.python.org/cpython ${PYTHON_DIR}
+	try hg clone http://hg.python.org/cpython ${PYTHON_DIR}
 fi
-( cd ${PYTHON_DIR} && hg pull && hg update && hg checkout ${PYTHON_VERSION})
+( try cd ${PYTHON_DIR} && hg pull && hg update && hg checkout ${PYTHON_VERSION} )
 
 ################################ Boost ########################################
 BOOST_VERSION=1_55_0
@@ -67,17 +74,17 @@ BOOST_DIR=Boost_${BOOST_VERSION}
 debug "Updating ${BOOST_DIR}"
 if [ ! -d ${BOOST_DIR} ]
 then
-	svn co http://svn.boost.org/svn/boost/tags/release/${BOOST_DIR} ${BOOST_DIR}
+	try svn co http://svn.boost.org/svn/boost/tags/release/${BOOST_DIR} ${BOOST_DIR}
 else
-	( cd ${BOOST_DIR} && svn update )
+	( try cd ${BOOST_DIR} && svn update )
 fi
 
 ################################ SDL ##########################################
 debug "Updating SDL"
-[ ! -d SDL ] && hg clone http://hg.libsdl.org/SDL
-( cd SDL && hg pull && hg update )
-[ ! -d SDL_image ] && hg clone http://hg.libsdl.org/SDL_image
-( cd SDL_image && hg pull && hg update )
+[ ! -d SDL ] && try hg clone http://hg.libsdl.org/SDL
+( try cd SDL && hg pull && hg update )
+[ ! -d SDL_image ] && try hg clone http://hg.libsdl.org/SDL_image
+( try cd SDL_image && hg pull && hg update )
 
 ############################ Bullet physics ###################################
 debug "Update BulletPhysics"
@@ -87,8 +94,8 @@ BULLET_TARBALL="${BULLET_NAME}.tgz"
 BULLET_URL="https://bullet.googlecode.com/files/${BULLET_TARBALL}"
 
 [ ! -d  "${BULLET_NAME}" ] && (
-	[ ! -f "${BULLET_TARBALL}" ] && fetch "${BULLET_URL}" "${BULLET_TARBALL}"
-	tar xf ${BULLET_TARBALL}
+	[ ! -f "${BULLET_TARBALL}" ] && try fetch "${BULLET_URL}" "${BULLET_TARBALL}"
+	try tar xf ${BULLET_TARBALL}
 )
 
 ################################ CURL #########################################
@@ -99,8 +106,8 @@ CURL_TARBALL="${CURL_NAME}.tar.gz"
 CURL_URL="http://curl.haxx.se/download/${CURL_TARBALL}"
 
 [ ! -d "${CURL_NAME}" ] && (
-	[ ! -f "${CURL_TARBALL}" ] && fetch "${CURL_URL}" "${CURL_TARBALL}"
-	tar xf "${CURL_TARBALL}"
+	[ ! -f "${CURL_TARBALL}" ] && try fetch "${CURL_URL}" "${CURL_TARBALL}"
+	try tar xf "${CURL_TARBALL}"
 )
 
 ############################### LIBIDN ########################################
@@ -111,8 +118,8 @@ LIBIDN_TARBALL=${LIBIDN_NAME}.tar.gz
 LIBIDN_URL=ftp://alpha.gnu.org/pub/gnu/libidn/${LIBIDN_TARBALL}
 
 [ ! -d "${LIBIDN_NAME}" ] && (
-	[ ! -f "${LIBIDN_TARBALL}" ] && fetch "${LIBIDN_URL}" "${LIBIDN_TARBALL}"
-	tar xf "${LIBIDN_TARBALL}"
+	[ ! -f "${LIBIDN_TARBALL}" ] && try fetch "${LIBIDN_URL}" "${LIBIDN_TARBALL}"
+	try tar xf "${LIBIDN_TARBALL}"
 )
 
 ############################## OpenSSL ########################################
@@ -123,8 +130,8 @@ OPENSSL_TARBALL=${OPENSSL_NAME}.tar.gz
 OPENSSL_URL=https://www.openssl.org/source/${OPENSSL_TARBALL}
 
 [ ! -d "${OPENSSL_NAME}" ] && (
-	[ ! -f "${OPENSSL_TARBALL}" ] && fetch "${OPENSSL_URL}" "${OPENSSL_TARBALL}"
-	tar xf "${OPENSSL_TARBALL}"
+	[ ! -f "${OPENSSL_TARBALL}" ] && try fetch "${OPENSSL_URL}" "${OPENSSL_TARBALL}"
+	try tar xf "${OPENSSL_TARBALL}"
 )
 
 ################################ ZLIB #########################################
@@ -135,8 +142,8 @@ ZLIB_TARBALL=${ZLIB_NAME}.tar.gz
 ZLIB_URL=http://zlib.net/${ZLIB_TARBALL}
 
 [ ! -d "${ZLIB_NAME}" ] && (
-	[ ! -f "${ZLIB_TARBALL}" ] && fetch "${ZLIB_URL}" "${ZLIB_TARBALL}"
-	tar xf "${ZLIB_TARBALL}"
+	[ ! -f "${ZLIB_TARBALL}" ] && try fetch "${ZLIB_URL}" "${ZLIB_TARBALL}"
+	try tar xf "${ZLIB_TARBALL}"
 )
 
 ################################ BZIP2 ########################################
@@ -147,8 +154,8 @@ BZIP2_TARBALL=${BZIP2_NAME}.tar.gz
 BZIP2_URL=http://www.bzip.org/${BZIP2_VERSION}/${BZIP2_TARBALL}
 
 [ ! -d "${BZIP2_NAME}" ] && (
-	[ ! -f "${BZIP2_TARBALL}" ] && fetch "${BZIP2_URL}" "${BZIP2_TARBALL}"
-	tar xf "${BZIP2_TARBALL}"
+	[ ! -f "${BZIP2_TARBALL}" ] && try fetch "${BZIP2_URL}" "${BZIP2_TARBALL}"
+	try tar xf "${BZIP2_TARBALL}"
 )
 
 exit 0
