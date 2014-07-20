001c347604e064854d9dacc1cb87ebe1842b81ee
Raphaël Londeix <raphael.londeix@infinit.io>
2014-07-21 00:34:57 +0200

    cube.app.entity: Make controllers more friendly to use.

--- src/cubeapp/game/entity/component/controller.py
+++ src/cubeapp/game/entity/component/controller.py
@@ -4,6 +4,15 @@ from . import Component
 class Controller(Component):
     systems = [Event]
 
-    def __init__(self, controller, name = None):
+    def __init__(self, controller, channels = None, name = None):
         super().__init__(name)
         self.controller = controller
+        if channels is not None:
+            self.channels = channels
+        elif hasattr(controller, 'channels'):
+            self.channels = controller.channels
+        else:
+            raise Exception("No channels were provided")
+
+    def __call__(self, ev, delta):
+        return self.controller(ev, delta)
--- src/cubeapp/game/entity/system/event.py
+++ src/cubeapp/game/entity/system/event.py
@@ -2,11 +2,16 @@
 from . import System
 
 class Event(System):
+    """Add given components to the event_manager.
+
+    Compatible components must have a "channels" list attribute and should
+    be callable with (event, elapsed_time) paremeters.
+    """
 
     def __init__(self, event_manager, name = None):
         super().__init__(name)
         self.event_manager = event_manager
 
     def init_component(self, entity, component):
-        self.event_manager.add(component.controller)
+        self.event_manager.add(component)
 
