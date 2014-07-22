328641d09b37613d6167183c8ed7fe23c2ff8d27
Raphaël Londeix <raphael.londeix@infinit.io>
2014-07-22 09:29:54 +0200

    WIP

--- src/cube/gl/camera.cpp
+++ src/cube/gl/camera.cpp
@@ -1,33 +1,85 @@
 #include "camera.hpp"
 #include "matrix.hpp"
+
+#include <etc/assert.hpp>
 #include <etc/print.hpp>
+
+#include <glm/gtx/quaternion.hpp>
 
 namespace cube { namespace gl { namespace camera {
 
-	Camera&
-	Camera::rotate(units::Angle const horizontal_angle,
-	               units::Angle const vertical_angle) ETC_NOEXCEPT
+	struct Camera::Impl
 	{
-		_hrot += horizontal_angle;
-		while (_hrot < units::rad(0))
-			_hrot += units::rad(3.14159265f*2);
-		while (_hrot > units::rad(3.14159265f*2))
-			_hrot -= units::rad(3.14159265f*2);
+		vec3 position;
+		glm::quat yaw; // up vector
+		glm::quat pitch; // right vector
+		glm::quat roll; // front vector
+	};
 
-		_vrot += vertical_angle;
-		if (_vrot > units::rad(3.14159f / 2))
-			_vrot = units::rad(3.14159f / 2 - 0.01f);
-		if (_vrot < units::rad(-3.14159f / 2))
-			_vrot = units::rad(-3.14159f / 2 + 0.01f);
+	Camera::Camera()
+		: _this{new Impl}
+	{}
 
-		_front = vector::rotate_x(vec3{0, 0, -1}, units::deg_value(_vrot));
-		_front = vector::rotate_y(_front, units::deg_value(_hrot));
-		_up = vector::rotate_x(vec3{0, 1, 0}, units::deg_value(_vrot));
-		_up = vector::rotate_y(_up, units::deg_value(_hrot));
+	Camera::~Camera()
+	{}
 
-		double dot = vector::dot(_up, _front);
-		assert(dot < 1e-7);
+	Camera::vec3 const& Camera::position() const ETC_NOEXCEPT
+	{ return _this->position; }
+
+	Camera& Camera::position(vec3 const& position) ETC_NOEXCEPT
+	{ _this->position = position; return *this; }
+
+	Camera& Camera::look_at(vec3 const& position) ETC_NOEXCEPT
+	{
+		ETC_ASSERT_NEQ(_this->position, position);
+		vec3 target = glm::normalize(position - _this->position);
+
 		return *this;
 	}
+
+	Camera::vec3 Camera::front() const ETC_NOEXCEPT
+	{ return glm::axis(_this->roll); }
+
+	Camera::vec3 Camera::up() const ETC_NOEXCEPT
+	{ return glm::axis(_this->yaw); }
+
+	Camera::vec3 Camera::right() const ETC_NOEXCEPT
+	{ return glm::axis(_this->pitch); }
+
+	Camera& Camera::move(vec3 const& v) ETC_NOEXCEPT
+	{ _this->position += v; return *this; }
+
+	//Camera&
+	//Camera::rotate(units::Angle const horizontal_angle,
+	//               units::Angle const vertical_angle) ETC_NOEXCEPT
+	//{
+	//	_hrot += horizontal_angle;
+	//	while (_hrot < units::rad(0))
+	//		_hrot += units::rad(3.14159265f*2);
+	//	while (_hrot > units::rad(3.14159265f*2))
+	//		_hrot -= units::rad(3.14159265f*2);
+
+	//	_vrot += vertical_angle;
+	//	if (_vrot > units::rad(3.14159f / 2))
+	//		_vrot = units::rad(3.14159f / 2 - 0.01f);
+	//	if (_vrot < units::rad(-3.14159f / 2))
+	//		_vrot = units::rad(-3.14159f / 2 + 0.01f);
+
+	//	_front = vector::rotate_x(vec3{0, 0, -1}, units::deg_value(_vrot));
+	//	_front = vector::rotate_y(_front, units::deg_value(_hrot));
+	//	_up = vector::rotate_x(vec3{0, 1, 0}, units::deg_value(_vrot));
+	//	_up = vector::rotate_y(_up, units::deg_value(_hrot));
+
+	//	double dot = vector::dot(_up, _front);
+	//	assert(dot < 1e-7);
+	//	return *this;
+	//}
+
+	//void Camera::look_at(vec3 const& position) ETC_NOEXCEPT
+	//{
+	//	_front = vector::normalize(position - _position);
+	//	vec3 right = this->right();
+	//	_up = vector::normalize(vector::cross(right, _front));
+	//}
 
 }}}
--- src/cube/gl/camera.hpp
+++ src/cube/gl/camera.hpp
@@ -16,44 +16,20 @@ namespace cube { namespace gl { namespace camera {
 		typedef vector::Vector3f vec3;
 
 	private:
-		vec3            _position;
-		vec3            _front;
-		vec3            _up;
-		units::Angle    _hrot;
-		units::Angle    _vrot;
+		struct Impl;
+		std::unique_ptr<Impl> _this;
 
 	public:
-		Camera() ETC_NOEXCEPT
-		{ this->rotate(units::deg(0.0f), units::deg(0.0f)); }
+		Camera();
+		~Camera();
 
-		inline
-		vec3 const& position() const ETC_NOEXCEPT
-		{ return _position; }
-
-		inline
-		void position(vec3 const& position) ETC_NOEXCEPT
-		{ _position = position; }
-
-		inline
-		vec3 const& front() const ETC_NOEXCEPT
-		{ return _front; }
-
-		inline
-		vec3 const& up() const ETC_NOEXCEPT
-		{ return _up; }
-
-		inline
-		vec3 right() const ETC_NOEXCEPT
-		{ return vector::normalize(vector::cross(this->_front, this->_up)); }
-
-		Camera& rotate(units::Angle const horizontal_angle,
-		               units::Angle const vertical_angle) ETC_NOEXCEPT;
-
-		inline Camera& move(vec3 const& v) ETC_NOEXCEPT
-		{
-			_position += v;
-			return *this;
-		}
+		vec3 const& position() const ETC_NOEXCEPT;
+		Camera& position(vec3 const& position) ETC_NOEXCEPT;
+		Camera& look_at(vec3 const& position) ETC_NOEXCEPT;
+		vec3 front() const ETC_NOEXCEPT;
+		vec3 up() const ETC_NOEXCEPT;
+		vec3 right() const ETC_NOEXCEPT;
+		Camera& move(vec3 const& v) ETC_NOEXCEPT;
 	};
 
 }}}
