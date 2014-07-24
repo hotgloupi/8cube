93b521f5c0b17d19c565722b52c111b29b084619
Raphaël Londeix <raphael.londeix@infinit.io>
2014-07-24 17:38:03 +0200

    cube.units: Bind add/sub for angle type.

--- src/cube/units/angle_test.py
+++ src/cube/units/angle_test.py
@@ -6,7 +6,20 @@ from .angle import Angle, rad, deg, rad_value, deg_value
 
 class _(Case):
 
-    def test_angle(self):
+    def test_null(self):
         a = Angle()
         self.assertEqual(rad_value(a), 0)
         self.assertEqual(deg_value(a), 0)
+
+    def test_sum(self):
+        self.assertAlmostEqual((deg(180) + rad(-3.14)).degree, 0, 1)
+        self.assertAlmostEqual((deg(180) - rad(3.14)).degree, 0, 1)
+
+    def test_value(self):
+        self.assertEqual(deg(180).degree, 180)
+        self.assertEqual(deg(-190).degree, -190)
+        self.assertEqual(rad(12).radian, 12)
+
+    def test_convert(self):
+        self.assertAlmostEqual(deg(180).radian, rad(3.14159265).radian, 1e-5)
+
