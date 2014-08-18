52a2279c6a32d4e5ceac3e0be4c68b20ed191734
Raphaël Londeix <raphael.londeix@infinit.io>
2014-08-18 05:40:21 +0200

    etc.meta: Add an helper to select function pointers of an overloaded function.

---
+++ src/etc/meta/select_overload.cpp
@@ -0,0 +1,46 @@
+#include "select_overload.hpp"
+
+#include <etc/test.hpp>
+
+namespace {
+
+	using etc::meta::select_overload;
+
+	class A
+	{
+		void f(int) {}
+		void f(float) {}
+	};
+
+
+	ETC_TEST_CASE(methods)
+	{
+		ETC_ASSERT_EQ(
+			select_overload<int>::of(&A::f),
+			static_cast<void (A::*)(int)>(&A::f)
+		);
+		ETC_ASSERT_EQ(
+			select_overload<float>::of(&A::f),
+			static_cast<void (A::*)(float)>(&A::f)
+		);
+	}
+
+	namespace a
+	{
+		void f(int) {}
+		void f(float) {}
+	}
+
+	ETC_TEST_CASE(functions)
+	{
+		ETC_ASSERT_EQ(
+			select_overload<int>::of(&a::f),
+			static_cast<void (*)(int)>(&a::f)
+		);
+		ETC_ASSERT_EQ(
+			select_overload<float>::of(&a::f),
+			static_cast<void (*)(float)>(&a::f)
+		);
+	}
+
+}
---
+++ src/etc/meta/select_overload.hpp
@@ -0,0 +1,16 @@
+#ifndef  ETC_META_SELECT_OVERLOAD_HPP
+# define ETC_META_SELECT_OVERLOAD_HPP
+
+namespace etc { namespace meta {
+
+	template<typename... Args>
+	struct select_overload
+	{
+		template<typename Ret, typename Class>
+		static auto of(Ret (Class::*fn)(Args...)) -> decltype(fn)
+		{ return fn; }
+	};
+
+}}
+
+#endif
