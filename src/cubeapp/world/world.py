2bcd42b36913f0787b13a1f4643a5f976fe03902
Raphaël Londeix <raphael.londeix@celoxica.com>
2014-08-27 17:38:44 +0200

    cubeapp.world: Remove dead code.

--- src/cubeapp/world/world.py
+++ src/cubeapp/world/world.py
@@ -15,7 +15,7 @@ from .chunk import Chunk
 coord_type = gl.Vector3il
 
 class World:
-    def __init__(self, storage, generator, renderer):
+    def __init__(self, renderer, storage = Storage(), generator = Generator()):
         assert isinstance(storage, Storage)
         assert isinstance(generator, Generator)
         self.__storage = storage
@@ -25,65 +25,18 @@ class World:
         self.__tree = tree.Tree(self.__tree_level)
         Chunk.prepare(renderer)
         self.referential = gl.Vector3il()
-        self.__frustum_view = None
-        attr = gl.make_vba
-        self.__frustum = gl.frustum.Frustumd(units.deg(45), 640.0 / 480.0,0.1,20)
-        self.__frustum_colors_vb = renderer.new_vertex_buffer([
-            attr(
-                gl.ContentKind.color,
-                [
-                    gl.Color3f('#300'),
-                    gl.Color3f('#300'),
-                    gl.Color3f('#300'),
-                    gl.Color3f('#300'),
-
-                    gl.Color3f('#030'),
-                    gl.Color3f('#030'),
-                    gl.Color3f('#030'),
-                    gl.Color3f('#030'),
-
-                    gl.Color3f('#083'),
-                    gl.Color3f('#083'),
-                    gl.Color3f('#083'),
-                    gl.Color3f('#083'),
-
-                    gl.Color3f('violet'),
-                    gl.Color3f('violet'),
-                    gl.Color3f('violet'),
-                    gl.Color3f('violet'),
-
-                    gl.Color3f('purple'),
-                    gl.Color3f('purple'),
-                    gl.Color3f('purple'),
-                    gl.Color3f('purple'),
-
-                    gl.Color3f('#840'),
-                    gl.Color3f('#840'),
-                    gl.Color3f('#840'),
-                    gl.Color3f('#840'),
-                ],
-                gl.ContentHint.static_content
-            )
-        ])
         self.__nodes_to_render = []
         self.__nodes_to_render_found = []
-        #self.__frustum_view = self.__frustum.view(self.__renderer)
 
-    def _update(self, delta, player, projection_matrix):
-        with cube.performance_section("app.WorldUpdate"):
-            pass
-    def update(self, delta, player, projection_matrix):
+    def update(self, delta, camera, referential):
+        self.referential = referential
         self.__player = player
         self.__pos = gl.vec3d(
-            player.world_position.x + player.position.x / Chunk.size,
-            player.world_position.y + player.position.y / Chunk.size,
-            player.world_position.z + player.position.z / Chunk.size,
+            referential.x + camera.position.x / Chunk.size,
+            referential.y + camera.position.y / Chunk.size,
+            referential.z + camera.position.z / Chunk.size,
         )
-        self.__frustum.update(
-            self.__player.camera.front,
-            self.__player.camera.up
-        )
-        self.referential = self.__player.world_position
+        self.__frustum.update(camera.front, camera.up)
 
         if not hasattr(self, 'tree_thread'):
             self.tree_thread = threading.Thread(target=self._update_nodes)
@@ -120,20 +73,20 @@ class World:
 
     @cube.check_performance("app.WorldRender")
     def render(self, painter):
-        if self.__frustum_view is not None:
-            assert False
-            with painter.bind([Chunk.sp, self.__frustum_colors_vb]):
-                state = painter.push()
-                state.translate(
-                    -self.referential.x * Chunk.size,
-                    -self.referential.y * Chunk.size,
-                    -self.referential.z * Chunk.size,
-                )
-                state.scale(
-                    Chunk.size, Chunk.size, Chunk.size
-                )
-                Chunk.sp.update(state)
-                painter.draw([self.__frustum_view])
+        #if self.__frustum_view is not None:
+        #    assert False
+        #    with painter.bind([Chunk.sp, self.__frustum_colors_vb]):
+        #        state = painter.push()
+        #        state.translate(
+        #            -self.referential.x * Chunk.size,
+        #            -self.referential.y * Chunk.size,
+        #            -self.referential.z * Chunk.size,
+        #        )
+        #        state.scale(
+        #            Chunk.size, Chunk.size, Chunk.size
+        #        )
+        #        Chunk.sp.update(state)
+        #        painter.draw([self.__frustum_view])
 
         if self.__nodes_to_render != self.__nodes_to_render_found:
             self.__nodes_to_render = self.__nodes_to_render_found[:]
