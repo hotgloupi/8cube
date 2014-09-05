c9bf074d7bb6be29e56e5d95ca3d1c1b6c164572
Raphaël Londeix <raphael.londeix@celoxica.com>
2014-09-05 17:23:32 +0200

    cubeapp.game.event: Allow manager to be cleared, handle delayed events.

--- src/cubeapp/game/event/manager.py
+++ src/cubeapp/game/event/manager.py
@@ -7,14 +7,29 @@ class Manager:
     """Store and forward events to registered controllers."""
 
     def __init__(self, tick_channel = Channel('tick')):
-        self.__queue = []
-        self.__controllers = dict()
         self.__tick = tick_channel
         self.__tick_event = Event(self.__tick)
+        self.__delay_channel = Channel()
+        self.clear()
 
-    def push(self, event):
+    def clear(self):
+        self.__cleared = True
+        self.__queue = []
+        self.__delayed_queue = []
+        self.__controllers = dict()
+        self.register(lambda ev, elapsed: ev.cb(elapsed), self.__delay_channel)
+
+    def call_later(self, cb, delay):
+        self.__delayed_queue.append(
+            (delay, Event(self.__delay_channel, cb = cb))
+        )
+
+    def push(self, event, delay = None):
         assert event.channel != self.__tick
-        self.__queue.append(event)
+        if delay:
+            self.__delayed_queue.append((time.time() + delay, event))
+        else:
+            self.__queue.append(event)
 
     def emplace(self, controller_class, *args, **kw):
         """Construct and add a controller the given class."""
@@ -47,12 +62,27 @@ class Manager:
             assert controller in l
             l.remove(controller)
 
+
     def poll(self, time_elapsed):
+        self.__cleared = False
         queue = self.__queue[:]
         self.__queue = []
+        delayed_queue = self.__delayed_queue[:]
+        self.__delayed_queue = []
+        for ts, event in delayed_queue:
+            ts -= time_elapsed
+            if ts < 0:
+                queue.append(event)
+            else:
+                self.__delayed_queue.append((ts, event))
+
         for event in queue:
             for controller in self.__controllers.get(event.channel.name, []):
                 controller(event, time_elapsed)
+                if self.__cleared:
+                    return
+
+
         for controller in self.__controllers.get(self.__tick.name, []):
             controller(self.__tick_event, time_elapsed)
 
