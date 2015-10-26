bd5ebe8269e9e56f501c7a18e9e84f8129cf51f0
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-10-26 20:34:01 +0100

    cube.gl.renderer.opengl: Do not expose TexStorage2D

--- src/cube/gl/renderer/opengl/_opengl.hpp
+++ src/cube/gl/renderer/opengl/_opengl.hpp
@@ -129,7 +129,7 @@ namespace cube { namespace gl { namespace renderer { namespace opengl {
 		_CUBE_GL_OPENGL_WRAP(TexCoordPointer);
 		_CUBE_GL_OPENGL_WRAP(TexImage2D);
 		_CUBE_GL_OPENGL_WRAP(TexParameteri);
-		_CUBE_GL_OPENGL_WRAP(TexStorage2D);
+		//_CUBE_GL_OPENGL_WRAP(TexStorage2D);
 		_CUBE_GL_OPENGL_WRAP(TexSubImage2D);
 		_CUBE_GL_OPENGL_WRAP(Uniform1f);
 		_CUBE_GL_OPENGL_WRAP(Uniform1i);
